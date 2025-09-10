d83cd62dc5f51c900b0967076f48d795939b32d9
Anee006 <aneesha.sibyj@gmail.com>
2025-09-10 23:47:56 +0530

    subtree of another

--- Binary Trees/Subtree of another tree.cpp
+++ Binary Trees/Subtree of another tree.cpp
@@ -10,3 +10,55 @@
 // (use the logic of "isSame Tree" Q to check whether subroot we found inside main tree and the original subroot are identical)
 
 //first check if root (of main tree) = subroot, then try to find subroot in the left subtree and then try finding it in right sub tree.
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int val)
+        {
+            data = val;
+            left = right = NULL;
+        }
+};
+
+bool isIdentical(Node* root1, Node* root2)
+{
+    if(root1 == NULL || root2 == NULL)
+    {
+        return root1 == root2;
+    }
+
+    bool leftSame = isIdentical(root1->left, root2->left);
+    bool rightSame = isIdentical(root1->right, root2->right);
+
+    return leftSame && rightSame && root1->data == root2->data;
+
+}
+
+bool isSubtree(Node* root, Node* subRoot)
+{
+    //base case:
+    if(root == NULL || subRoot == NULL)
+    {
+        return root == subRoot; //if returns true means subroot is equal to root, else subroot does not exist in the main tree
+    }
+
+    //checking whether the root and subroot are same
+    if(root->data == subRoot->data && isIdentical(root, subRoot))
+    {
+        return true;  //means the given subroot exists in the main tree
+    }
+
+    //if the root of main tree is != subroot then check for the subroot in the left subtree, if not found in left subtree then check in the 
+    //right subtree. 
+    //If either one of the left/right subtree returns true means we have found the subroot in the main root.
+    return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+    
+}
