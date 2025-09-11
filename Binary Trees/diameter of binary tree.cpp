af6f789855ca83e39deb77fbd82010198e4d1ecb
Anee006 <aneesha.sibyj@gmail.com>
2025-09-11 21:56:04 +0530

    Updated test

---
+++ Binary Trees/diameter of binary tree.cpp
@@ -0,0 +1,13 @@
+//DIAMETER OF BINARY TREE (LeetCode 543)
+
+//Given the root of a binary tree, return the length of the diameter of the tree.
+
+//The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
