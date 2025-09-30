e039950b1940e25a40a7e891440f5bc3c3897ee3
Anee006 <aneesha.sibyj@gmail.com>
2025-09-30 21:35:01 +0530

    Convert sorted arr to BST

--- Binary search tree/sorted array to BST.cpp
+++ Binary search tree/sorted array to BST.cpp
@@ -1,3 +1,4 @@
+// #newnotes
 //CONVERT SORTED ARRAY TO BST (LeetCode 108)
 
 //Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
