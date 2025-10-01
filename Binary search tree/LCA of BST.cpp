96f13900b98a892f42b83697bb0dcabf1db2c09f
Anee006 <aneesha.sibyj@gmail.com>
2025-10-01 08:46:37 +0530

    LCA of BST

--- Binary search tree/LCA of BST.cpp
+++ Binary search tree/LCA of BST.cpp
@@ -8,6 +8,14 @@
 //Output: 6
 
 
+//LCA --> is the first node in whose lst and rst both p and q exist.
+
+//to find p and q we will use BST properties.
+//case 1: if(root->val > p->val && root->val > q->val) --> ans will be in the LST
+//case 2: else if(root->val < p->val && root->val < q->val) --> ans will be in the RST
+//case 3: p exists in the LST and q exists in the RST --> this is the condition for an LCA  --> return root as the ans
+//case 4: when either of p or q is the LCA --> is handled in case 3 itself --> root will be the ans in this case as well
+
 #include <iostream>
 using namespace std;
 
@@ -27,10 +35,51 @@ class Node
 
 Node* LCA(Node* root, Node* p, Node* q)
 {
+    if(root == NULL) return NULL; //base case
 
+    // case 1:
+    if(root->data > p->data && root->data > q->data)  //means p & q are both less than root --> ans is in LST
+    {
+        return LCA(root->left, p, q);
+    }
+
+    //case 2:
+    else if(root->data < p->data && root->data < q->data) //means p & q are both greater than root --> ans is in RST
+    {
+        return LCA (root->right, p, q);
+    }
+
+    //case 3:
+    else
+    {
+        return root; 
+    }
 }
+//TC = O(height of tree)
+// Since, it is a balanced BST, TC = O(log n).
 
 int main()
 {
+    Node* root = new Node(6);
+    root->left = new Node(2);
+    root->right = new Node(8);
+    root->left->left = new Node(0);
+    root->left->right = new Node(4);
+    root->right->left = new Node(7);
+    root->right->right = new Node(9);
+    root->left->right->left = new Node(3);
+    root->left->right->right = new Node(5);
+
+    Node* p1 = root->left->right->left; //p = 3
+    Node* q1 = root->left->right->right;  // q = 5
+    Node* lca_1= LCA(root, p1, q1);
+
+    cout << "LCA of " << p1->data << " and " << q1->data << " is: " << lca_1->data << endl;
+
+    Node* p2 = root; // p = 6
+    Node* q2 = root->right->right; // q = 9
+    Node* lca_2 = LCA(root, p2, q2);
+
+    cout << "LCA of " << p2->data << " and " << q2->data << " is: " << lca_2->data << endl;
     return 0;
 }
