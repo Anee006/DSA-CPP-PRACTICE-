b9009cf2261811d7e6608a38ff20085173439b0a
Anee006 <aneesha.sibyj@gmail.com>
2025-11-15 23:40:26 +0530

    Schedule courses

---
+++ Graphs/Course schedule.cpp
@@ -0,0 +1,23 @@
+// COURSE SCHEDULE (LeetCode 207)
+// (using topological sort)
+
+// There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. 
+// You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+// For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+// Return true if you can finish all courses. Otherwise, return false.
+
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+bool canFinish(int numCourses, vector<vector<int>>& prerequisites)
+{
+
+}
+
+int main()
+{
+    return 0;
+}
