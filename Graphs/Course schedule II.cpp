46e47f6c0b99779235e5e31b0b810edcf3208e42
Anee006 <aneesha.sibyj@gmail.com>
2025-11-16 16:42:01 +0530

    course sch 2

---
+++ Graphs/Course schedule II.cpp
@@ -0,0 +1,11 @@
+// COURSE SCHEDULE II (LeetCode 210)
+
+// There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. 
+// You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+// Return the ordering of courses you should take to finish all courses. If there are many valid answers, return any of them. 
+// If it is impossible to finish all courses, return an empty array.
+
+// Input: numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+// Output: [0,2,1,3]
+// Another correct ordering is [0,1,2,3].
