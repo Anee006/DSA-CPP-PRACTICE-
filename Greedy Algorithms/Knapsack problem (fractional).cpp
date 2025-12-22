e1a8394eb490e0e883308e7936083b91568fead9
Anee006 <aneesha.sibyj@gmail.com>
2025-12-22 10:08:31 +0530

    Knapsack

---
+++ Greedy Algorithms/Knapsack problem (fractional).cpp
@@ -0,0 +1,19 @@
+// FRACTIONAL KNAPSACK PROBLEM (using Greedy Approach)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct Item
+{
+    int profit, weight;
+    double ratio;
+};
+
+// Comparator function to sort items by profit/weight ratio
+
+int main()
+{
+    return 0;
+}
