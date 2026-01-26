1012b933c00941917785f665f48269cddee0998f
Anee006 <aneesha.sibyj@gmail.com>
2026-01-26 12:24:46 +0530

    sorting odd and even placed nums

---
+++ Sorting Algo/modified sorting.cpp
@@ -0,0 +1,3 @@
+// to sort all even-placed numbers in increasing and odd-place numbers in decreasing order. 
+// The modified array contains all sorted even-placed numbers followed by reverse sorted odd-placed numbers.
+
