297be21bf6ea29bd943688352ad43530b6e62fa2
Anee006 <aneesha.sibyj@gmail.com>
2026-01-26 12:55:50 +0530

    Merge sorted halves into a single sorted array

---
+++ Sorting Algo/merge sorted halves into single array.cpp
@@ -0,0 +1,3 @@
+// Given an integer array of which both first half and second half are sorted. Task is to merge two sorted halves of array into single sorted array.
+// (this is the merge step of merge sort algo).
+
