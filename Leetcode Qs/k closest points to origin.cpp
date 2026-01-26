8a74b5a1fde5eb7ee5d7a5991ce67cc4381bc95e
Anee006 <aneesha.sibyj@gmail.com>
2026-01-26 14:05:15 +0530

    k closest pts

---
+++ Leetcode Qs/k closest points to origin.cpp
@@ -0,0 +1,5 @@
+// K Closest Points to Origin (LeetCode 973)
+
+// List of points have been given on 2D Plane. Calculate K closest points to the origin (0,0) (Consider
+// euclidean distance to find the distance between two points). 
+// Write a code to return the answer in any order. The solution is guaranteed to be unique.
