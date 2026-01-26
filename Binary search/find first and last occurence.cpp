079e647a92853f036fb5819878524b7dd7e16151
Anee006 <aneesha.sibyj@gmail.com>
2026-01-26 15:25:38 +0530

    find first and last occur

---
+++ Binary search/find first and last occurence.cpp
@@ -0,0 +1,2 @@
+// Consider a sorted array A of n elements. The array A may have repetitive/duplicate elements. 
+// For a given target element T, design an efficient algorithm to find T’s first and last occurrence in the array A. Print a message if an element was not present in the array.
