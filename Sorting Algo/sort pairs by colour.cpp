8b3f136129f6dac2c48fcf64b03192c13a658ecb
Anee006 <aneesha.sibyj@gmail.com>
2026-01-26 13:22:52 +0530

    sort pairs by colors

---
+++ Sorting Algo/sort pairs by colour.cpp
@@ -0,0 +1,6 @@
+// we are given n pairs of items as input, where the first item is a number and the second item is one of three colours (red, blue, or yellow). 
+// Further assume that the items are sorted by number.
+// Give a O(n) algorithm to sort the items by colour (all reds before all blues before all yellows) such that the numbers for identical colours stay sorted.
+
+// Input: (1, blue), (3, red), (4, blue), (6, yellow), (9, red)
+// Output: (3, red), (9, red), (1, blue), (4, blue), (6, yellow)
