ebc0441fbb79f87747ffb97c1c66bdb82d5bd1d9
Anee006 <aneesha.sibyj@gmail.com>
2026-02-25 18:00:11 +0530

    string reverse

--- Recursion (Backtracking)/print string in rev order.cpp
+++ Recursion (Backtracking)/print string in rev order.cpp
@@ -5,18 +5,18 @@
 using namespace std;
 void print_reverse(string str, int idx) //str is the string to be reversed and idx is the variable to store the index.
 {
-    if(idx==0) //base case
+    if(idx == 0) //base case
     {
-        cout<<str.at(idx);
+        cout << str.at(idx);
         return;
     }
 
-    cout<<str.at(idx); //to print the character at that particular index
-    print_reverse(str,idx-1); //as we want to print in reverse order
+    cout << str.at(idx); //to print the character at that particular index
+    print_reverse(str, idx-1); //as we want to print in reverse order
 }
 int main()
 {
-    string str= "abcd";
+    string str = "abcd";
     print_reverse(str, str.length()-1);
     
     return 0;
