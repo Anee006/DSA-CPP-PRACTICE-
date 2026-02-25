4376cf3b185d853e2501f50b95d501b2a5a8c7da
Anee006 <aneesha.sibyj@gmail.com>
2026-02-25 22:36:59 +0530

    generate parentheses

--- Recursion (Backtracking)/generate balanced parentheses.cpp
+++ Recursion (Backtracking)/generate balanced parentheses.cpp
@@ -12,13 +12,41 @@ Input: n = 1
 Output: ["()"]
 */
 
+// LOGIC:
+// We maintain: 
+// open -> number of '(' used
+// close -> number of ')' used
+
+// Conditions:
+// We can add '(' if open < N
+// We can add ')' if close < open
+
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
-vector<string> generateParentheses(int n) {
+void generate(int n, int open, int close, string curr, vector<string>& ans) {
+    if(curr.length() == 2*n) {  // means we have generated all valid pairs of ()
+        ans.push_back(curr);
+        return;
+    }
 
+    if(open < n) {
+        generate(n, open+1, close, curr + "(", ans); // add "("
+    }
+    
+    if(close < open) {
+        generate(n, open, close+1, curr + ")", ans); // add ")"
+    }
+}
+
+vector<string> generateParentheses(int n) {
+    vector<string> ans;
+
+    generate(n, 0, 0, "", ans);
+
+    return ans;
 }
 
 int main() {
@@ -26,8 +54,12 @@ int main() {
     vector<string> ans = generateParentheses(1);
     for(string val : ans) cout << val << endl;
 
+    cout << "\n\n";
+
     ans = generateParentheses(3);
     for(string val : ans) cout << val << endl;
 
     return 0;
 }
+
+// TC = O(2^n)
