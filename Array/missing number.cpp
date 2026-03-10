a70ddde7a78db5493603bcab8e72490d51732f1b
Anee006 <aneesha.sibyj@gmail.com>
2026-03-10 23:15:20 +0530

    missing number

--- Array/missing number.cpp
+++ Array/missing number.cpp
@@ -6,13 +6,26 @@
 // Output: 2
 // Explanation: n = 3 since numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
 
+// LOGIC:
+// use the formula --> sum of 1st n numbers = n(n+1)/2
+// then subtract the actual sum of the array.
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
 
 int missingNumber(vector<int>& nums) {
+    int n = nums.size();
 
+    int expected = n * (n + 1)/2;
+    int actual = 0;
+
+    for(int num : nums) {
+        actual += num; // to find sum of the array.
+    }
+
+    return expected - actual;
 }
 
 int main() {
