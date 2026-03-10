6daae62851f314970b24f605fd77e23aff1a5f2b
Anee006 <aneesha.sibyj@gmail.com>
2026-03-10 23:00:25 +0530

    missing num

---
+++ Hashing problems/missing number.cpp
@@ -0,0 +1,24 @@
+// LeetCode 268
+
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+// Input: nums = [3,0,1]
+// Output: 2
+// Explanation: n = 3 since numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int missingNumber(vector<int>& nums) {
+
+}
+
+int main() {
+    vector<int> arr = {3,0,1};
+
+    cout << "Missing number is: " << missingNumber(arr);
+
+    return 0;
+}
