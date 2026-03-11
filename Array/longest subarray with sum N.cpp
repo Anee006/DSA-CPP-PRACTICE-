38b46d877e62a6ce4be28019d3d88324c930f44f
Anee006 <aneesha.sibyj@gmail.com>
2026-03-11 21:22:08 +0530

    longest subarray

---
+++ Array/longest subarray with sum N.cpp
@@ -0,0 +1,22 @@
+// Longest Sum with subarray Sum N. (LeetCode 325)
+// using hashing (prefix sum + hashmap)
+
+// Find the length of the longest subarray whose sum equals k.
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+int maxSubarrayLength(vector<int>& nums, int k) {
+
+}
+
+int main() {
+    vector<int> nums = {1, -1, 5, -2, 3};
+    int k = 3;
+
+    cout << maxSubarrayLength(nums, k);
+
+    return 0;
+}
