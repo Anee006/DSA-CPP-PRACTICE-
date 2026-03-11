7e9aefb97eeca39fdfb6f8f3c46b1fbf0ed827db
Anee006 <aneesha.sibyj@gmail.com>
2026-03-11 21:54:01 +0530

    longest subarray with sum N

--- Array/longest subarray with sum N.cpp
+++ Array/longest subarray with sum N.cpp
@@ -9,7 +9,26 @@
 using namespace std;
 
 int maxSubarrayLength(vector<int>& nums, int k) {
+    unordered_map<int, int> m; // to store prefix sum, first idx where it appears
+    int prefix = 0, ans = 0;
 
+    // Prefix sum = 0 occurs at index -1 (before the array starts) --> helps detect subarrays that start from index 0.
+    m[0] = -1;
+
+    for(int i=0; i<nums.size(); i++) {
+        prefix += nums[i]; // finding prefix sum
+
+        // Check if a valid subarray exists:
+        if(m.find(prefix - k) != m.end()) {
+            ans = max(ans, i - m[prefix - k]); // update max length (Subarray length: current index - previous index)
+        }
+
+        // Store prefix sum if it is new
+        if(m.find(prefix) == m.end()) {
+            m[prefix] = i;
+        }
+    }
+    return ans;
 }
 
 int main() {
