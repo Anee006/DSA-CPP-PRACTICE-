3aa6b3e093f070375c13ec5726ef862d33d4d6ba
Anee006 <aneesha.sibyj@gmail.com>
2026-03-11 23:12:31 +0530

    number complement

---
+++ Bit Manipulation/Number complement.cpp
@@ -0,0 +1,28 @@
+// LeetCode 476
+
+// The complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
+
+// Given an integer num, return its complement.
+
+// Example:
+/*
+Input: num = 5
+Output: 2
+Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So output is 2.
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findComplement(int n) {
+
+}
+
+int main() {
+    int n = 5;
+
+    cout << findComplement(n);
+
+    return 0;
+}
