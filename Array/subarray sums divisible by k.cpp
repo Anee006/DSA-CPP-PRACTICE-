6df74407c00dc18dbcbaa7e0587bea0ada2e527e
Anee006 <aneesha.sibyj@gmail.com>
2026-03-11 22:30:23 +0530

    subarray sums divisble by k

--- Array/subarray sums divisible by k.cpp
+++ Array/subarray sums divisible by k.cpp
@@ -11,12 +11,39 @@ Explanation: There are 7 subarrays with a sum divisible by k = 5:
 [4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
 */
 
+// LOGIC:
+// Use prefix sums. If 2 prefix sums have the same Count when divided by k, then subarray b/w them is divisible by k.
+// If (prefix[i] % k = prefix[j] % k) then (prefix[j] - prefix[i]) % k = 0
+// so subarray (i+1...j) has sum divisible by k
+
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 int subarraysDivByK(vector<int>& nums, int k) {
-        
+    unordered_map<int, int> remainderCount; // map stores freq of remainders --> (remainder, count)
+
+    remainderCount[0] = 1; // handles subarrays starting from index 0
+
+    int prefixSum = 0, result = 0;
+
+    for(int i=0; i<nums.size(); i++) {
+        prefixSum += nums[i]; // update prefix sum
+
+        int remainder = prefixSum % k; // find Count of prefix sum with k
+
+        if(remainder < 0) { // handle -ve Count cases
+            remainder += k; 
+        }
+
+        // if remainder appeared before, means there exists subarrays divisible by k
+        if(remainderCount.find(remainder) != remainderCount.end()) {
+            result += remainderCount[remainder]; // add how many times this remainder appeared
+        }
+        remainderCount[remainder]++;
+    }
+    return result;
 }
 
 int main() {
@@ -27,3 +54,5 @@ int main() {
 
     return 0;
 }
+
+// TC = O(n) , SC = O(n)
