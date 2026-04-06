ca9ab661513bef9123b0a0ed5620c2d6d5f8abef
Anee006 <aneesha.sibyj@gmail.com>
2026-04-06 23:25:57 +0530

    graph colorinh

--- Greedy Algorithms/Graph Coloring.cpp
+++ Greedy Algorithms/Graph Coloring.cpp
@@ -10,6 +10,25 @@ int main() {
     vector<int> color(V, -1);
     color[0] = 0;
 
+    for(int u = 1; u < V; u++) {
+        set<int> used;
+
+        for(int v : adj[u]) {
+            if(color[v] != -1) {
+                used.insert(color[v]);
+            }
+        }
+
+        int c = 0;
+        while(used.count(c)) c++;
+
+        color[u] = c;
+    }
+
+    for(int i=0; i < V; i++) {
+        cout << i;
+    }
+
     return 0;
 }
 
