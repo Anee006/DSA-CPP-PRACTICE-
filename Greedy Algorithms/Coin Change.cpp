efee27341e54c2db86cf66e4dc60ea180743b453
Anee006 <aneesha.sibyj@gmail.com>
2026-04-06 18:48:50 +0530

    coin change updated

--- Greedy Algorithms/Coin Change.cpp
+++ Greedy Algorithms/Coin Change.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
     vector<int> coins = {1,2,5,10};
     int amount = 28;
 
+    sort(coins.rbegin(), coins.rend()); // sort in desc order
+
+    for(int coin : coins) {
+        while(amount >= coin) {
+            cout << coin << " ";
+            amount -= coin;
+        }
+    }
+
     return 0;
-    
 }
