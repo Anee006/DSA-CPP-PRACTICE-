d0752648974e80367879ed0fdaf47d26e0ce16e5
Anee006 <aneesha.sibyj@gmail.com>
2026-04-06 20:59:25 +0530

    job scheduling with deadlines

--- Greedy Algorithms/Job Scheduling.cpp
+++ Greedy Algorithms/Job Scheduling.cpp
@@ -5,6 +5,34 @@
 #include <algorithm>
 using namespace std;
 
+struct Job {
+    int id, deadline, profit;
+};
+
+bool compare(Job a, Job b) {
+    return a.profit > b.profit; // sort in desc order
+}
+
 int main() {
+    vector<Job> jobs = {{1,2,100}, {2,1,19}, {3,2,27}, {4,1,25}, {5,3,15}}; // (job_id, deadline, profit)
+
+    sort(jobs.begin(), jobs.end(), compare);
+
+    int maxDeadline = 3; // max of all deadlines given
+    vector<int> slot(maxDeadline + 1, -1);
+    int profit = 0;
+
+    for(auto job : jobs) {
+        for(int i = job.deadline; i > 0; i--) {
+            if(slot[i] == -1) {
+                slot[i] = job.id;
+                profit += job.profit;
+                break;
+            }
+        }
+    }
+
+    cout << "Max Profit: " << profit << endl;
+
     return 0;
 }
