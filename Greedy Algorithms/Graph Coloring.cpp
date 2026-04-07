b690b5944c606e43ccb044161fb7a3fd5331a6b9
Anee006 <aneesha.sibyj@gmail.com>
2026-04-07 07:34:33 +0530

    added comment to graph coloring

--- Greedy Algorithms/Graph Coloring.cpp
+++ Greedy Algorithms/Graph Coloring.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     int V = 5;
 
-    vector<vector<int>> adj = {{1,2}, {0,2,3}, {0,1,3}, {1,2,4}, {3}};
+    vector<vector<int>> adj = {{1,2}, {0,2,3}, {0,1,3}, {1,2,4}, {3}};  // adj[i] = list of vertices connected to vertex i
     vector<int> color(V, -1);
     color[0] = 0;
 
