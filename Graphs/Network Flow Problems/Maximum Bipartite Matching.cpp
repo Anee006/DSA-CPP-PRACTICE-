5b63e2c9e15a3c0dda43b48830bd2ace0b044ba4
Anee006 <aneesha.sibyj@gmail.com>
2026-04-07 22:45:03 +0530

    max bipartite matching using greedy and dfs

--- Graphs/Network Flow Problems/Maximum Bipartite Matching.cpp
+++ Graphs/Network Flow Problems/Maximum Bipartite Matching.cpp
@@ -23,6 +23,36 @@ If job is free -> assign directly, else -> try to reassign the current applicant
 #include <vector>
 using namespace std;
 
+// DFS fn to find a job for applicant u
+bool canAssign(int u, int N, vector<vector<int>>& graph, vector<bool>& visited, vector<int>& jobAssigned) {
+    for(int v = 0; v < N; v++) {
+        // If applicant u is interested in job v and not visited
+        if(graph[u][v] && !visited[v]) {
+            visited[v] = true;
+
+            // If job is not assigned OR we can reassign the previous applicant
+            if(jobAssigned[v] == -1 || canAssign(jobAssigned[v], N, graph, visited, jobAssigned)) {
+                jobAssigned[v] = u; // assign job v to applicant u
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int maxMatch(vector<vector<int>>& graph, int N, int M) {
+    vector<int> jobAssigned(N, -1); // // jobAssigned[j] = applicant assigned to job j
+    int result = 0;
+
+    // try to assign each applicant
+    for(int i=0; i < M; i++) {
+        vector<bool> visited(N, false); // reset visited for each applicant
+
+        if(canAssign(i, N, graph, visited , jobAssigned)) result++;
+    }
+    return result;
+}
+
 int main() {
     /*
     graph[i][j] = 1 → applicant i interested in job j
@@ -33,6 +63,9 @@ int main() {
     Applicant 3 → Job 2
     */
 
+    int M = 4; // Number of applicants
+    int N = 4; // Number of jobs
+
     vector<vector<int>> graph = {
         {1, 1, 0, 0},
         {0, 1, 1, 0},
@@ -40,5 +73,10 @@ int main() {
         {0, 0, 1, 0}
     };
 
+    cout << "Max applicants assigned jobs: " << maxMatch(graph, N, M);
+
     return 0;
 }
+
+// TC = O(M * N²)
+// SC = O(N)
