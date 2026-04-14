ab3c9ca3f973b1e07fca76f784c98d65b9dd54fa
Anee006 <aneesha.sibyj@gmail.com>
2026-04-14 18:21:09 +0530

    max no. of edge disjoint paths using max flow

--- Graphs/Network Flow Problems/Max no. of edge-disjoint paths.cpp
+++ Graphs/Network Flow Problems/Max no. of edge-disjoint paths.cpp
@@ -14,11 +14,59 @@
 #include <queue>
 using namespace std;
 
-int maxFiles(vector<vector<int>>& graph, int s , int t) {
+bool bfs(vector<vector<int>>& residual, int V, int s, int t, vector<int>& parent) {
+    vector<bool> visited(V, false);
+    queue<int> q;
 
+    q.push(s);
+    visited[s] = true;
+    parent[s] = -1;
+
+    while(!q.empty()) {
+        int u = q.front();
+        q.pop();
+
+        for(int v = 0; v < V; v++) {
+            if(!visited[v] && residual[u][v] > 0) {
+                q.push(v);
+                parent[v] = u;
+                visited[v] = true;
+            }
+        }
+    }
+    return visited[t];
+}
+
+int maxFiles(vector<vector<int>>& graph, int V, int s , int t) {
+    vector<vector<int>> residual = graph;
+    vector<int> parent(V);
+    int maxFlow = 0;
+
+    while(bfs(residual, V, s, t, parent)) {
+        int pathFlow = INT_MAX;
+
+        for(int v = t; v != s; v = parent[v]) {
+            int u = parent[v];
+            pathFlow = min(pathFlow, residual[u][v]);
+        }
+
+        // update residual graph
+        for(int v = t; v != s; v = parent[v]) {
+            int u = parent[v];
+            residual[u][v] -= pathFlow;
+            residual[v][u] += pathFlow;
+        }
+        maxFlow += pathFlow;
+    }
+    return maxFlow;
 }
 
 int main() {
+
+    int V = 6;
+
+    // Example graph (1 -> edge exists, capacity = 1)
+
     vector<vector<int>> graph = {
         {0,1,1,0,0,0},
         {0,0,1,1,0,0},
@@ -30,7 +78,10 @@ int main() {
 
     int s = 0, t = 5;
 
-    cout << "Max no. of files: " << maxFiles(graph, s, t);
+    cout << "Max no. of files: " << maxFiles(graph, V, s, t);
 
     return 0;
 }
+
+// TC = O(V * E^2)
+// SC = O(V^2)
