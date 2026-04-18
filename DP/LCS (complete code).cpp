c63a1b3857f22de7b9dde0f62b52ef3b219d166e
Anee006 <aneesha.sibyj@gmail.com>
2026-04-18 09:36:37 +0530

    print lcs length and the lcs string

--- DP/LCS (complete code).cpp
+++ DP/LCS (complete code).cpp
@@ -7,18 +7,58 @@
 // "↑" (from top)
 // "←" (from left)
 
-// to reconstruct the actual subsequence
-void printLCS() {
-
-}
-
 #include <iostream>
 #include <vector>
 using namespace std;
 
+// to reconstruct the actual subsequence
+void printLCS(vector<vector<char>>& b, string X, int i, int j) {
+    if(i == 0 || j == 0) return;
+
+    if(b[i][j] == 'd') {
+        printLCS(b, X, i-1, j-1); // diagonal
+        cout << X[i-1]; // print only the diagonal 
+    }
+    else if(b[i][j] == 'u') {
+        printLCS(b, X, i-1, j); // ↑
+    }
+    else {
+        printLCS(b, X, i, j-1); // ←
+    }
+}
+
 int main() {
     string X = "abcde";
-    string Y = "ace";
+    string Y = "ace"; // this is also the LCS
+
+    int n = X.length(), m = Y.length();
+
+    vector<vector<int>> c(n+1, vector<int>(m+1, 0));
+    vector<vector<char>> b(n+1, vector<char>(m+1));
+
+    // build tables
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= m; j++) {
+
+            if(X[i-1] == Y[j-1]) {
+                c[i][j] = c[i-1][j-1] + 1;
+                b[i][j] = 'd'; // diagonal ↖
+            }
+            else if(c[i-1][j] >= c[i][j-1]) {
+                c[i][j] = c[i-1][j];
+                b[i][j] = 'u'; // up ↑ 
+            }
+            else {
+                c[i][j] = c[i][j-1];
+                b[i][j] = 'l'; // left ←
+            }
+        }
+    }
+
+    cout << "Length of LCS = " << c[n][m] << endl;
+
+    cout << "LCS: ";
+    printLCS(b, X, n, m);
 
     return 0;
 }
