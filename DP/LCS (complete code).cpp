c1a723ffceff08a8ca7313ce260376e3cfbb42ea
Anee006 <aneesha.sibyj@gmail.com>
2026-04-18 09:17:35 +0530

    lcs string

---
+++ DP/LCS (complete code).cpp
@@ -0,0 +1,24 @@
+// prints both length of LCS and the LCS string
+
+// Here,
+// c[i][j] -> DP table storing LCS length
+// b[i][j] -> to store directions: 
+// "↖" (diagonal, match)
+// "↑" (from top)
+// "←" (from left)
+
+// to reconstruct the actual subsequence
+void printLCS() {
+
+}
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    string X = "abcde";
+    string Y = "ace";
+
+    return 0;
+}
