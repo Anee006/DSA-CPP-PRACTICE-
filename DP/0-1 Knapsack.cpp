573d8c4febad07e6a625394ec17b031cc74673d0
Anee006 <aneesha.sibyj@gmail.com>
2026-04-20 16:42:26 +0530

    0/1 knapsack using DP

--- DP/0-1 Knapsack.cpp
+++ DP/0-1 Knapsack.cpp
@@ -2,7 +2,55 @@
 #include <vector>
 using namespace std;
 
+// fn to solve and return selected items
+void knapsack(int n, int W, vector<int>& wt, vector<int>& val) {
+
+    // create DP table:
+    vector<vector<int>> dp(n+1, vector<int>(W+1, 0));
+
+    for(int i = 1; i <= n; i++) {
+        for(int w = 0; w <= W; w++) {
+            if(wt[i-1] <= w) {
+                dp[i][w] = max(dp[i-1][w], val[i-1] + dp[i-1][w - wt[i-1]]);
+            } 
+            else {
+                dp[i][w] = dp[i-1][w]; // copy the val in the above cell (row)
+            }
+        }
+    }
+
+    cout << "Max value: " << dp[n][W] << endl;
+
+    // Backtrack to find the selected items --> the "x" vector
+    vector<int> x(n, 0); // sol vector
+    int i = n, w = W; // start from the last cell in the 2D matrix
+
+    while(i > 0 && w > 0) {
+        // if val came from including the item
+        if(dp[i][w] != dp[i-1][w]) {
+            x[i-1] = 1; // include item
+            w = w - wt[i-1]; // reduce capacity
+        }
+        i--;
+    }
+
+    // print the sol vector
+    cout << "Selected items (x): ";
+    for(int i=0; i<n; i++) {
+        cout << x[i] << " ";
+    }
+    cout << endl;
+}
+
 int main() {
-    
+    int n = 3; // no. of items
+    int W = 50; // max capacity of knapsack
+
+    vector<int> wt = {10, 20, 30};   // weight of each item in the knapsack
+    vector<int> val = {60, 100, 120};  // benefit of each item in the knapsack
+
+    knapsack(n, W, wt, val);
     return 0;
 }
+
+// TC = O(n * W)
