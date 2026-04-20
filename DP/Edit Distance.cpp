23adaff0d83f40867ef3b2216e00e56f03cf4047
Anee006 <aneesha.sibyj@gmail.com>
2026-04-20 19:26:25 +0530

    Edit distance using DP

--- DP/Edit Distance.cpp
+++ DP/Edit Distance.cpp
@@ -1,5 +1,5 @@
 // LeetCode 72
-// string Editing problem
+// String Editing problem
 
 // Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
 // You have the following three operations permitted on a word:
@@ -18,10 +18,30 @@ rose -> ros (remove 'e')
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int minDistance(string word1, string word2) {
+    int m = word1.size(), n = word2.size();
 
+    vector<vector<int>> dp(m+1, vector<int>(n+1));
+
+    for(int i = 0; i <= m; i++) dp[i][0] = i;
+    for(int j = 0; j <= n; j++) dp[0][j] = j;
+
+    for(int i = 1; i <= m; i++) {
+        for(int j = 1; j <= n; j++) {
+            if(word1[i-1] == word2[j-1]) {
+                dp[i][j] = dp[i-1][j-1];
+            }
+            else {
+                dp[i][j] = 1 + min({dp[i-1][j], // delete
+                                dp[i][j-1],     // insert
+                                dp[i-1][j-1]}); // replace
+            }
+        }
+    }
+    return dp[m][n];
 }
 
 int main() {
@@ -31,4 +51,4 @@ int main() {
     return 0;
 }
 
-// TC: O(n + m), SC: O(m)
+// TC: O(m * n), SC: O(m * n)
