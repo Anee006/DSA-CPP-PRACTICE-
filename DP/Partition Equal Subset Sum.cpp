3f5d6ed415b92b294c867c38c7b4a0b019d6a99d
Anee006 <aneesha.sibyj@gmail.com>
2026-04-20 17:00:47 +0530

    partition equal subset sum using 0/1 knapsack logic

--- DP/Partition Equal Subset Sum.cpp
+++ DP/Partition Equal Subset Sum.cpp
@@ -11,7 +11,7 @@ Explanation: The array can be partitioned as [1, 5, 5] and [11].
 */
 
 // LOGIC:
-// Compute total sum. If odd -> not possible
+// Compute total sum. If odd -> not possible (cannot divide into 2 equal subsets)
 // Target = sum / 2
 // Use DP: Either take element OR don't take. If dp[target] == true -> possible
 
@@ -20,18 +20,39 @@ Explanation: The array can be partitioned as [1, 5, 5] and [11].
 using namespace std;
 
 bool canPartition(vector<int>& nums) {
+    int sum = 0;
 
+    // find the total sum
+    for(int x : nums) sum += x;
+
+    // if sum is odd, cannot divide into 2 equal subsets
+    if(sum % 2 != 0) return false;
+
+    int target = sum/2;
+
+    // create DP array --> dp[j] = can we make sum "j" using elements?
+    vector<bool> dp(target + 1, false);
+
+    dp[0] = true; // base case
+
+    // 0/1 knapsack
+    for(int i=0; i<nums.size(); i++) {
+        // traverse backwards
+        for(int j = target; j >= nums[i]; j--) {
+            dp[j] = dp[j] || dp[j - nums[i]];
+        }
+    }
+    return dp[target];
 }
 
 int main() {
     vector<int> nums = {1, 5, 11, 5};
 
-    if(canPartition(nums)) cout << "Can be partitioned into 2 subsets with equal sum.";
-    else cout << "Cannot be partitioned";
+    if(canPartition(nums)) cout << "True";
+    else cout << "False";
 
     return 0;
 }
-
 
 // TC = O(n * target)
 // SC = O(target)
