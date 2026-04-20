5eb63df4900effac97e851290580d84e5658497f
Anee006 <aneesha.sibyj@gmail.com>
2026-04-20 18:47:31 +0530

    MCM

---
+++ DP/Matrix Chain Multiplication.cpp
@@ -0,0 +1,12 @@
+// Idea is to: minimize multiplication cost
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> arr = {10, 20, 30, 40};
+    int n = arr.size();
+
+    return 0;
+}
