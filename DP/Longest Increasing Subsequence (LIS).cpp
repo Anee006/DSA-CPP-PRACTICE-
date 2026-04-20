37d4fc2ca801b0935682f037b014e35e12acd7cc
Anee006 <aneesha.sibyj@gmail.com>
2026-04-20 19:08:03 +0530

    LIS using DP

--- DP/Longest Increasing Subsequence (LIS).cpp
+++ DP/Longest Increasing Subsequence (LIS).cpp
@@ -10,10 +10,22 @@ Explanation: The longest increasing subsequence is [2,3,7,101], therefore the le
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int LIS(vector<int>& nums) {
+    int n = nums.size();
 
+    vector<int> dp(n, 1); // all values in dp array initialized with 1
+
+    for(int i=1; i<n; i++) {
+        for(int j=0; j<i; j++) {
+            if(nums[i] > nums[j]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+    return *max_element(dp.begin(), dp.end());
 }
 
 int main() {
@@ -22,3 +34,5 @@ int main() {
     cout << "Length of LIS: " << LIS(nums);
     return 0;
 }
+
+// TC = O(n^2), SC = O(n)
