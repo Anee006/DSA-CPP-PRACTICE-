a78f83942ca5c3d9053578c608a8f0f2350860cb
Anee006 <aneesha.sibyj@gmail.com>
2026-04-20 18:32:20 +0530

    combination sum 4

---
+++ DP/Combination Sum IV.cpp
@@ -0,0 +1,35 @@
+// LeetCode 377
+// similar to coin change II problem but in this: order matters (permutations)
+
+// Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
+
+/*
+Input: nums = [1,2,3], target = 4
+Output: 7
+Explanation:
+The possible combination ways are:
+(1, 1, 1, 1)
+(1, 1, 2)
+(1, 2, 1)
+(1, 3)
+(2, 1, 1)
+(2, 2)
+(3, 1)
+Note that different sequences are counted as different combinations.
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int combinationSum4(vector<int>& nums, int target) {
+
+}
+
+int main() {
+    vector<int> nums = {1,2,3};
+    int target = 4;
+    
+    cout << "Number of possible combinations: " << combinationSum4(nums, target);
+    return 0;
+}
