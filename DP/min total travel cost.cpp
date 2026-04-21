91eea6cc4e0ae18e6348e949c6e206c66a39bb2b
Anee006 <aneesha.sibyj@gmail.com>
2026-04-21 18:33:56 +0530

    finding minimum total cost to travel from A to D using DP

--- DP/min total travel cost.cpp
+++ DP/min total travel cost.cpp
@@ -16,6 +16,7 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
@@ -26,5 +27,28 @@ int main() {
         {70, 90, 150}    // C -> D
     };
 
+    int n = cost.size();
+
+    // dp[i][j] --> min cost to reach segment "i" using ride "j"
+    vector<vector<int>> dp(n, vector<int>(3,0));
+
+    // initialize the first segment (A → B) so that we have previous results to work with
+    for(int j=0; j<3; j++) dp[0][j] = cost[0][j]; // base case
+
+    for(int i=1; i<n; i++) {
+
+        // dp[i][j] = cost[i][j] + min(other two from previous row)  --> so that we don't pick same ride as before
+
+        dp[i][0] = cost[i][0] + min(dp[i-1][1], dp[i-1][2]); // bike
+        dp[i][1] = cost[i][1] + min(dp[i-1][0], dp[i-1][2]); // auto
+        dp[i][2] = cost[i][2] + min(dp[i-1][0], dp[i-1][1]); // cab
+    }
+
+    int ans = min({dp[n-1][0], dp[n-1][1], dp[n-1][2]}); // ans is the min val of last segment
+
+    cout << "Min cost: " << ans;  // ans = 200 (50 + 80 + 70)
+
     return 0;
 }
+
+// TC = O(n x 3) = O(n)
