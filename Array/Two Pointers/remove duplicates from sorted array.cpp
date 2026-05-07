94655576be9b4b3e184d3d64b6921ec0266202df
Anee006 <aneesha.sibyj@gmail.com>
2026-05-07 16:42:44 +0530

    solved using two pointers

--- Array/Two Pointers/remove duplicates from sorted array.cpp
+++ Array/Two Pointers/remove duplicates from sorted array.cpp
@@ -1,4 +1,5 @@
 // LeetCode 26
+// using 2 pointers (slow fast pointers)
 
 // Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
 // The relative order of the elements should be kept the same.
@@ -7,3 +8,41 @@
 
 // Input: nums = [1,1,2]
 // Output: 2, nums = [1,2,_]
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+    // edge case: when array is empty
+    if(nums.size() == 0) return 0;
+
+    int slow = 0; // slow pointer --> it is the position to place unqiue el
+
+    // fast pointer --> traverses the array
+    for(int fast = 1; fast < nums.size(); fast++) {
+
+        // if current el is different --> found a unique el
+        if(nums[fast] != nums[slow]) {
+            slow++;
+
+            // place unique el at next position
+            nums[slow] = nums[fast];
+        }
+    }
+    return slow+1; // no. of unique elements
+}
+
+int main() {
+    vector<int> nums = {1, 1, 2};
+    int k = removeDuplicates(nums);
+
+    cout << "Number of unique elements: " << k << endl;
+    cout << "\nArray after removing duplicates:\n";
+
+    for(int i = 0; i < k; i++) {
+        cout << nums[i] << " ";
+    }
+
+    return 0;
+}
