1aea934edda0b75ac4ac0c4c46b2627c2cb21efd
Anee006 <aneesha.sibyj@gmail.com>
2026-05-07 18:30:26 +0530

    solved using fixed sliding window

--- Array/Sliding window/maximum number of vowels in a substring of given length.cpp
+++ Array/Sliding window/maximum number of vowels in a substring of given length.cpp
@@ -3,6 +3,7 @@
 
 // Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
 // Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+// s consists of lowercase English letters.
 
 // Input: s = "abciiidef", k = 3
 // Output: 3
@@ -12,8 +13,32 @@
 #include <string>
 using namespace std;
 
+// to check if char is a vowel
+bool isVowel(char c) {
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+}
+
 int maxVowels(string s, int k) {
-        
+    int count = 0;
+
+    // count vowels in 1st window
+    for(int i=0; i<k; i++) {
+        if(isVowel(s[i])) count++;
+    }
+
+    int maxCount = count;
+
+    // sliding window
+    for(int i = k; i < s.length(); i++) {
+        // remove left char
+        if(isVowel(s[i-k])) count--;
+
+        // add right char
+        if(isVowel(s[i])) count++;
+
+        maxCount = max(maxCount, count);
+    }
+    return maxCount;
 }
 
 int main() {
