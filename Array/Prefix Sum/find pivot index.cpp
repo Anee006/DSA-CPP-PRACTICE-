e99b8d63b8c8cd9670672035eaf0c25e53505483
Anee006 <aneesha.sibyj@gmail.com>
2026-05-07 22:58:22 +0530

    pivot index

---
+++ Array/Prefix Sum/find pivot index.cpp
@@ -0,0 +1,34 @@
+// LeetCode 724
+// using prefix sum
+
+// Given an array of integers nums, calculate the pivot index of this array.
+// pivot idx is the idx where the sum of all the numbers strictly to the left of the idx is equal to the sum of all the numbers 
+// strictly to the index's right. If the index is on the left edge of the array, then the left sum is 0 because there are no 
+// elements to the left. This also applies to the right edge of the array.
+// Return the leftmost pivot index. If no such index exists, return -1.
+
+// Input: nums = [1,7,3,6,5,6]
+// Output: 3
+// Explanation: The pivot index is 3.
+// Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+// Right sum = nums[4] + nums[5] = 5 + 6 = 11
+
+// LOGIC:
+// find total sum of array
+// leftSum = sum before current idx, rightSum = totalSum - leftSum - currentElement
+// if both are equal --> return index.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int pivotIndex(vector<int>& nums) {
+        
+}
+
+int main() {
+    vector<int> nums = {1,7,3,6,5,6};
+
+    cout << "Pivot index: " << pivotIndex(nums);
+    return 0;
+}
