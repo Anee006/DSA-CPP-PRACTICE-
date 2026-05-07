bad5274baae90eafcb1bf5a525a39cd879372e6d
Anee006 <aneesha.sibyj@gmail.com>
2026-05-07 22:46:23 +0530

    changes file location

---
+++ Array/Prefix Sum/sub array sum equals k_2.cpp
@@ -0,0 +1,55 @@
+// #notes
+// LeetCode 560
+// Using Hashing & prefix sum  
+
+// Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+// Given arr= {9,4,20,3,10,5}, k=33
+// Sub arrays whose sum is 33: {9,4,20} and {20,3,10}
+// So total number of subarrays whose sum is 33 is 2, which is the ans.
+
+// To track the no. of prefix sums of[i-1], use an unordered_map, as in practical cases, it's TC is O(1).
+// Create, unordered_map <int,int>, where 1st int is the "prefix sum" & 2nd int is for it's "frequency".
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int subarraySum(vector<int>& arr, int k) {
+    int n = arr.size();
+    int count = 0; // to count no. of subarrays whose sum is equal to k
+    vector<int> prefixSum(n,0); // to store all prefix sums of the arr // size of this vector is n and initialised with 0
+
+    prefixSum[0] = arr[0]; // sum of 0th idx is the no. itself
+
+    for(int i=1; i<n; i++) { // starting from i=1 as we have already calculated for i=0
+        prefixSum[i]= prefixSum[i-1] + arr[i]; // adding the new val to already calc sum so far
+    }
+
+    unordered_map<int,int> m; // to store the prefixSum and its freq in the prefixSum vector
+
+    for(int j=0; j<n; j++) {
+        if(prefixSum[j]==k) count++; //means we have found the req subarray in the beginning itself
+
+        int val = prefixSum[j] - k; // val --> prefixSum[i-1]
+        if(m.find(val) != m.end()) { // means val is present in map already, i.e, we have found ans
+            count += m[val]; // m[val] means the freq of the prefixSum in the vector
+        }
+
+        if(m.find(prefixSum[j]) == m.end()) { // means prefixSum[j] does not exist in map
+            m[prefixSum[j]] = 0; // initially stored in map with freq of 0
+        }
+        m[prefixSum[j]]++;
+    }
+    return count;
+}
+
+int main() {
+    vector <int> nums= {9,4,20,3,10,5};
+    cout<< subarraySum(nums,33);
+
+    return 0;
+}
+
+// TC = O(n) and SC = O(n)
