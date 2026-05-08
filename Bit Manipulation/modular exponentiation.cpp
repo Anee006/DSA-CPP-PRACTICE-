2d25c7ad8733dcf99e467d0e8e6e0dc0ebf4268b
Anee006 <aneesha.sibyj@gmail.com>
2026-05-09 00:44:30 +0530

    Modular Exponentiation

---
+++ Bit Manipulation/modular exponentiation.cpp
@@ -0,0 +1,15 @@
+// given three integers x, n, M, compute (x^n) % M
+
+#include <iostream>
+using namespace std;
+
+int powMod(int x, int n, int M) {
+
+}
+
+int main() {
+    int x = 3, n = 2, M = 4;
+    cout << powMod(x, n, M); // returns (3^2)%4 --> 9 % 4 --> 1
+
+    return 0;
+}
