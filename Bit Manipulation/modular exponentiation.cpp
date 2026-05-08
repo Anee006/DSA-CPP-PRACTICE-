709eeea3fe2f51799a9b830216f62d2a5d123a38
Anee006 <aneesha.sibyj@gmail.com>
2026-05-09 00:51:16 +0530

    solved using modulo exponentiation

--- Bit Manipulation/modular exponentiation.cpp
+++ Bit Manipulation/modular exponentiation.cpp
@@ -4,7 +4,23 @@
 using namespace std;
 
 int powMod(int x, int n, int M) {
+    int res = 1;
 
+    while(n >= 1) { // loop until exponent becomes 0
+
+        // if n is odd, multiply result by current x & take modulo
+        if(n & 1) {
+            res = (res * x) % M;
+            n--; // reduce exponent by 1 to make it even
+        }
+
+        // n is even, square the base & half the exponent
+        else {
+            x = (x * x) % M;
+            n /= 2;
+        }
+    }
+    return res;
 }
 
 int main() {
@@ -13,3 +29,5 @@ int main() {
 
     return 0;
 }
+
+// TC = O(log n) , SC = O(1)
