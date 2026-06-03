e2d172490696b8bb4336a00a6f7cd477995ebc5e
Anee006 <aneesha.sibyj@gmail.com>
2026-06-03 16:43:52 +0530

    single number using xor

--- Leetcode Qs/find the single one.cpp
+++ Leetcode Qs/find the single one.cpp
@@ -1,49 +1,43 @@
-//Given an array of integers called "nums". In it, every element appears twice except for one. Find that single one.
-// let, nums= [4,1,2,1,2]
+// LeetCode 136
 
-#include <iostream>
-#include <vector>
-using namespace std;
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
-//Need to use BITWISE XOR operator for this.
-//Rules of BITWISE XOR:
+// Example:
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+
+// LOGIC:
+// use BITWISE XOR operator.
+// Rules of BITWISE XOR:
 // 0^0 = 0  //same, so cancels
 // 1^1 = 0  //same, so cancels
 // 0^1 = 1
 // 1^0 = 1
 
-// BITWISE XOR operator ke paas agar same value hoti hai toh wo usse cancel kardeta hai i.e, zero kar deta hai.
+// Given array: [4,1,2,1,2]
+// Binary form of 2 is 10.
+// so, 2^2 i.e, 10^10 = 0  (0^0 = 0 and  1^1 = 0)
+// So, for a number "n",
+// n^n = 0  and  n^0 = n
 
-//Given array: [4,1,2,1,2]
-//Binary form of 2 is 10.
-//so, 2^2 i.e, 10^10 = 0  (0^0 = 0 and  1^1 = 0)
 
-//So, for a number "n",
-//n^n =0  and  n^0=n
+#include <iostream>
+#include <vector>
+using namespace std;
 
-class solution
-{
-    public:
-        int singleNumber(vector <int> &nums)  //as we want the changes done to be reflected everywhere so we are using pass by ref here
-        {
-            int ans=0;
+int singleNumber(vector<int>& nums) {
+    int ans = 0;
 
-            for(int val: nums)
-            {
-                ans= ans^val;
-            }
-            return ans;
-        }
-};
+    for(int val: nums) {
+        ans ^= val;
+    }
+    return ans;
+}
 
-int main()
-{
+int main() {
 
     vector <int> nums= {4,1,2,1,2};
-
-    solution obj;
-    cout<<"Required ans is: "<< obj.singleNumber(nums) <<endl;
-
+    cout << singleNumber(nums) << endl;
 
     return 0;
 }
