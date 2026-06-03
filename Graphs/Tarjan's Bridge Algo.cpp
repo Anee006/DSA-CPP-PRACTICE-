d02d20f06aef3cd4ba98e95246f0cc89feb3e583
Anee006 <aneesha.sibyj@gmail.com>
2026-06-03 17:08:00 +0530

    tarjans algo

---
+++ Graphs/Tarjan's Bridge Algo.cpp
@@ -0,0 +1,6 @@
+// To find the bridges in an undirected graph.
+// A bridge is an edge whose removal increases the number of connected components.
+
+// Condition: low[child] > disc[parent]
+// Then: (parent,child) is a bridge.
+
