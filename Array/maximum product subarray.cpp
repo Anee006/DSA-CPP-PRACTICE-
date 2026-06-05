40c50908da7dc273ef7828eaba3e45935b703ea6
Anee006 <aneesha.sibyj@gmail.com>
2026-06-05 13:52:16 +0530

    max prod subarray solved by keeping track of Max and Min

--- Array/maximum product subarray.cpp
+++ Array/maximum product subarray.cpp
@@ -9,10 +9,26 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int maxProduct(vector<int>& nums) {
-        
+    int currMax = nums[0];
+    int currMin = nums[0]; // keep track of Min, bcoz multiplying 2 -ve nums can become a large +ve num
+    int result = nums[0];
+
+    for(int i = 1; i < nums.size(); i++) {
+        int num = nums[i];
+
+        // if num is -ve, swap currMax and currMin
+        if(num < 0) swap(currMax, currMin);
+
+        currMax = max(num, currMax * num);
+        currMin = min(num, currMin * num);
+
+        result = max(result, currMax);
+    }
+    return result;
 }
 
 int main() {
@@ -22,3 +38,6 @@ int main() {
 
     return 0;
 }
+
+// TC = O(n)
+// SC = O(1)
