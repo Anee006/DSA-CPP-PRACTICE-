da68a1d393df10481e44b995833216123e1d918a
Anee006 <aneesha.sibyj@gmail.com>
2026-06-05 13:45:17 +0530

    max product subarray

---
+++ Array/maximum product subarray.cpp
@@ -0,0 +1,24 @@
+// #newnotes
+// LeetCode 152
+
+// Given an integer array nums, find a subarray that has the largest product, and return the product.
+
+// Input: nums = [2,3,-2,4]
+// Output: 6
+// Explanation: [2,3] has the largest product 6. [2,4] is not a subarray.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxProduct(vector<int>& nums) {
+        
+}
+
+int main() {
+    vector<int> nums = {2,3,-2,4};
+    
+    cout << maxProduct(nums);
+
+    return 0;
+}
