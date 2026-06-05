6cbd5d6e1e2689ae725c3ce1f210a6485a2c4c56
Anee006 <aneesha.sibyj@gmail.com>
2026-06-05 10:28:53 +0530

    Longest Consecutive Sequence

---
+++ Hashing/Longest Consecutive Sequence.cpp
@@ -0,0 +1,24 @@
+// LeetCode 128
+
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+// Input: nums = [100,4,200,1,3,2]
+// Output: 4
+// Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+int longestConsecutive(vector<int>& nums) {
+        
+}
+
+int main() {
+    vector<int> nums = {100,4,200,1,3,2};
+
+    cout << longestConsecutive(nums);
+    
+    return 0;
+}
