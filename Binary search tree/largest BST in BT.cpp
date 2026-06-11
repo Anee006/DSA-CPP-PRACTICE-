d3869277f8d03192d7de75ffc662fddbf8de6681
Anee006 <aneesha.sibyj@gmail.com>
2026-06-11 20:44:17 +0530

    minor fix

--- Binary search tree/largest BST in BT.cpp
+++ Binary search tree/largest BST in BT.cpp
@@ -1,25 +1,24 @@
 // #newnotes
-//LARGEST BST IN BINARY TREE
+// LARGEST BST IN BINARY TREE
+// (logic is similar to that of validate BST Q)
 
-//(logic is similar to that of validate BST Q)
+// given a binary tree, return the size of the largest BST in the BT.
 
-//given a binary tree, return the size of the largest BST in the BT.
+// LOGIC OF BRUTE FORCE APPROACH (TC = O(n*n)) -->
+// for each node check whether it satisfies BST properties using "validate BST" logic.
+// if we find a valid BST we don't need to check further below that node bcoz the BSTs below it will be smaller in size
+// and according to the Q we need the "largest" BST.
 
-//LOGIC OF BRUTE FORCE APPROACH ( TC: O(n*n) ) -->
-//for each node check whether it satisfies BST properties using "validate BST" logic.
-//if we find a valid BST we don't need to check further below that node bcoz the BSTs below it will be smaller in size
-//and according to the Q we need the "largest" BST.
-
-//LOGIC OF OPTIMISED APPROACH ( TC: O(n) )
-//In a BST, left.Max < root->val < right.Min, where left.Max is the max val in lst and righ.tMin is the min val in rst.
-//check whether the left and right child of a node are BSTs or not. If both are BSTs, then root node will also be a BST.
-//Find the min and max values in the RST and LST.
-//Find the size of LST and RST in order to get the size of root.
+// LOGIC OF OPTIMISED APPROACH (TC = O(n))
+// In a BST, left.Max < root->val < right.Min, where left.Max is the max val in lst and righ.tMin is the min val in rst.
+// check whether the left and right child of a node are BSTs or not. If both are BSTs, then root node will also be a BST.
+// Find the min and max values in the RST and LST.
+// Find the size of LST and RST in order to get the size of root.
 
 
-//NOTE:
-//If a node is not a valid BST, it returns (-infinity as min value) and (+infinity as max val) to its upper node.
-//if a node is a valid BST, it returns (+infinity as min val) and (-infinity as max val) to its upper node.
+// NOTE:
+// If a node is not a valid BST, it returns (-infinity as min value) and (+infinity as max val) to its upper node.
+// if a node is a valid BST, it returns (+infinity as min val) and (-infinity as max val) to its upper node.
 
 #include <iostream>
 #include <vector>
@@ -39,11 +38,11 @@ class Node
         }
 };
 
-//each node will have this info with it
+// each node will have this info with it
 class Info
 {
     public:
-        int min, max, size; //here "size" means the size of the max BST we have found inside the BT
+        int min, max, size; // here "size" means the size of the max BST we have found inside the BT
 
         Info(int min, int max, int size)
         {
@@ -53,15 +52,15 @@ class Info
         }
 };
 
-//returns info of a node
+// returns info of a node
 Info helper(Node* root)
 {
-    if(root == NULL)  //base case --> in case of valid BST (single node is also a valid BST)
+    if(root == NULL)  // base case --> in case of valid BST (single node is also a valid BST)
     {
-        return Info(INT_MAX, INT_MIN, 0); //INT_MAX means (+infinity) and INT_MIN means (-infinity)
+        return Info(INT_MAX, INT_MIN, 0); // INT_MAX means (+infinity) and INT_MIN means (-infinity)
     }
 
-    Info left = helper(root->left); //to get info of LST
+    Info left = helper(root->left); // to get info of LST
     Info right = helper(root->right); //to get info of RST
 
     if(root->data > left.max && root->data < right.min) //is a valid BST
@@ -76,12 +75,12 @@ Info helper(Node* root)
     return Info(INT_MIN, INT_MAX, max(left.size, right.size)); //in case of invalid BST
 }
 
-//to find the largest BST in BT
+// to find the largest BST in BT
 int largestBSTinBT(Node* root)
 {
     Info info = helper(root);
 
-    return info.size; //this is the max BST size
+    return info.size; // this is the max BST size
 }
 
 int main()
