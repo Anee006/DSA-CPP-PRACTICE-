34fc1e7dc6b20026ed814a266ff51cd93f97c82a
Anee006 <aneesha.sibyj@gmail.com>
2026-06-24 16:17:28 +0530

    added TC and SC

--- Binary Trees/sum of left leaves.cpp
+++ Binary Trees/sum of left leaves.cpp
@@ -1,12 +1,11 @@
-// SUM OF LEFT LEAVES (LeetCode 404)
+// LeetCode 404
 
-//Given the root of a binary tree, return the sum of all left leaves.
+// Given the root of a binary tree, return the sum of all left leaves.
+// A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
 
-//A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
-
-//Input: root = [3,9,20,null,null,15,7]
-//Output: 24
-//Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 24
+// Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
 
 #include <iostream>
 using namespace std;
@@ -25,26 +24,20 @@ class Node
         }
 };
 
-int sumOfLeftLeaves(Node* root)
-{
+int sumOfLeftLeaves(Node* root) {
     if(root == NULL) return 0;
 
     int sum = 0;
 
-    //check if the left child exists and is a LEAF node
-    if(root->left != NULL && root->left->left == NULL && root->left->right == NULL)
-    {
+    // check if the left child exists and is a LEAF node
+    if(root->left != NULL && root->left->left == NULL && root->left->right == NULL) {
         sum += root->left->data;
     }
 
     return sum + sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
 }
 
-//TC = O(n)
-//SC = O(n)
-
-int main()
-{
+int main() {
     Node* root = new Node(3);
     root->left = new Node(9);
     root->right = new Node(20);
@@ -55,3 +48,6 @@ int main()
 
     return 0;
 }
+
+// TC = O(n)
+// SC = O(n)
