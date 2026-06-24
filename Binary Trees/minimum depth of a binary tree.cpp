f6033e3dd64046f8b69fef999508eec36a578ecb
Anee006 <aneesha.sibyj@gmail.com>
2026-06-24 16:03:24 +0530

    minor fixes

--- Binary Trees/minimum depth of a binary tree.cpp
+++ Binary Trees/minimum depth of a binary tree.cpp
@@ -1,12 +1,11 @@
-// MINIMUM DEPTH OF A BINARY TREE (LeetCode 111)
+// LeetCode 111
 
-//Given a binary tree, find its minimum depth.
+// Given a binary tree, find its minimum depth.
+// The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
 
-//The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
-
-//Input: root = [3,9,20,null,null,15,7]
-//Output: 2 
-//Explanation: shortest path is of 3 and 9 edge. So min depth = 2
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 2 
+// Explanation: shortest path is of 3 and 9 edge. So min depth = 2
 
 #include <iostream>
 using namespace std;
@@ -25,29 +24,24 @@ class Node
         }
 };
 
-int minDepth(Node* root)
-{
+int minDepth(Node* root) {
     if(root == NULL) return 0;
 
     // If the left child is missing, continue searching down the right side.
-    if(root->left == NULL)
-    {
+    if(root->left == NULL) {
         return 1 + minDepth(root->right);
     }
 
     // If the right child is missing, continue searching down the left side.
-    if(root->right == NULL)
-    {
+    if(root->right == NULL) {
         return 1 + minDepth(root->left);
     }
 
     // If both children exist, we take the shorter path.
     return 1 + min(minDepth(root->left), minDepth(root->right));
 }
-//TC = O(n)
 
-int main()
-{
+int main() {
     Node* root = new Node(3);
     root->left = new Node(9);
     root->right = new Node(20);
@@ -58,3 +52,5 @@ int main()
 
     return 0;
 }
+
+// TC = O(n)
