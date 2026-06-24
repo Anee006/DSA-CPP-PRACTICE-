e66569dca8a3802d1ba1e07628cbc7303e4de166
Anee006 <aneesha.sibyj@gmail.com>
2026-06-24 19:06:23 +0530

    minor fixes

--- Heap/max_priority_queue_1.cpp
+++ Heap/max_priority_queue_1.cpp
@@ -1,29 +1,28 @@
-//to build max priority queue (i.e, for max heap)
+// to build max priority queue (i.e, for max heap)
 
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
     priority_queue <int> p;
 
-    //to insert elements in priority_queue:
+    // to insert elements in priority_queue:
     p.push(10);
     p.push(20);
     p.push(11);
     p.push(18);
     p.push(15);
 
-    //to find size of priority_queue:
+    // to find size of priority_queue:
     cout << "Size: " << p.size() << endl;
 
-    //to find the element at top of heap -->should give the max element of heap
-    cout << "\nMax element: " << p.top() << endl; //prints 20
+    // to find the element at top of heap --> should give the max element of heap
+    cout << "\nMax element: " << p.top() << endl; // prints 20
 
-    //to delete an element from heap
-    p.pop(); //pops 20
-    cout << "Max element: " << p.top() << endl; //prints 18
+    // to delete an element from heap
+    p.pop(); // pops 20
+    cout << "Max element: " << p.top() << endl; // prints 18
 
     return 0;
 }
