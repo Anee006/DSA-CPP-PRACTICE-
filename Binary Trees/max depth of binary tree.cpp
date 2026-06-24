dc7280eae8e8e8062acdc126bb772f901eef5031
Anee006 <aneesha.sibyj@gmail.com>
2026-06-24 16:00:21 +0530

    minor fixes

--- Binary Trees/max depth of binary tree.cpp
+++ Binary Trees/max depth of binary tree.cpp
@@ -1,9 +1,9 @@
-// MAXIMUM DEPTH OF A BINARY TREE (LeetCode 104) 
-//(this uses the same logic as that to find the "height of a binary tree").
+// LeetCode 104
+// (this uses the same logic as that to find the "height of a binary tree").
 
-//Given the root of a binary tree, return its maximum depth.
+// Given the root of a binary tree, return its maximum depth.
 
-//A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+// A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
 #include <iostream>
 using namespace std;
@@ -22,25 +22,23 @@ class Node
         }
 };
 
-int maxDepth(Node* root)
-{
+int maxDepth(Node* root) {
     if(root == NULL) return 0;
 
     return 1 + max(maxDepth(root->left), maxDepth(root->right));
 }
 
-//TC = O(n) , n is the no. of nodes
-//SC = O(n)
-
-int main()
-{
+int main() {
     Node* root = new Node(3);
     root->left = new Node(9);
     root->right = new Node(20);
     root->right->left = new Node(15);
     root->right->right = new Node(7);
 
-    cout << "Max Depth of Tree is: " << maxDepth(root) << endl; //output is 3
+    cout << "Max Depth of Tree is: " << maxDepth(root) << endl; // output is 3
 
     return 0;
 }
+
+// TC = O(n) , n is the no. of nodes
+// SC = O(n)
