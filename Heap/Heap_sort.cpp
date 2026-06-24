77f952526094761988d18afdbdde7932b2155bf8
Anee006 <aneesha.sibyj@gmail.com>
2026-06-24 18:54:29 +0530

    added time complexities

--- Heap/Heap_sort.cpp
+++ Heap/Heap_sort.cpp
@@ -3,67 +3,51 @@
 #include <iostream>
 using namespace std;
 
-void Heapify(int arr[], int i, int n)
-{
+void Heapify(int arr[], int i, int n) {
     int largest = i;
     int left = 2*i + 1;
     int right = 2*i + 2;
 
-    if(left < n && arr[left] > arr[largest])
-    {
-        largest = left;
-    }
+    if(left < n && arr[left] > arr[largest]) largest = left;
 
-    if(right < n && arr[right] > arr[largest])
-    {
-        largest = right;
-    }
-
-    if(largest != i)
-    {
+    if(right < n && arr[right] > arr[largest]) largest = right;
+    
+    if(largest != i) {
         swap(arr[i], arr[largest]);
-        Heapify(arr, largest, n); //new index that needs to be put in its correct place is "largest"
+        Heapify(arr, largest, n); // new index that needs to be put in its correct place is "largest"
     }
 }
+// TC = O(log n)
 
-void buildMaxHeap(int arr[], int n)
-{
-    for(int i = n/2 - 1; i >=0; i--)
-    {
-        Heapify(arr, i, n);
-    }
+void buildMaxHeap(int arr[], int n) {
+    for(int i = n/2 - 1; i >= 0; i--) Heapify(arr, i, n);
 }
+// TC = O(n)
 
-void printHeap(int arr[], int n)
-{
-    for(int i=0; i<n; i++)
-    {
-        cout << arr[i] << " ";
-    }
+void printHeap(int arr[], int n) {
+    for(int i=0; i<n; i++) cout << arr[i] << " ";
     cout << endl;
 }
 
-//to sort the heap in ascending order
-void HeapSort(int arr[], int n)
-{
-    for(int i= n-1; i>0; i--)
-    {
+// to sort the heap in ascending order
+void HeapSort(int arr[], int n) {
+    for(int i = n-1; i > 0; i--) {
         swap(arr[i], arr[0]);
-        Heapify(arr, 0, i); //size is equal to "i"
+        Heapify(arr, 0, i); // size is equal to "i"
     }
 }
+// TC = O(n log n)
 
-int main()
-{
+int main() {
     int arr[] = {10, 3, 8, 9, 5, 13, 18, 14, 11, 70};
     int n = sizeof(arr)/ sizeof(arr[0]);
 
     buildMaxHeap(arr, n);
-    printHeap(arr, n);  //70 14 18 11 5 13 8 9 10 3 
+    printHeap(arr, n);  // 70 14 18 11 5 13 8 9 10 3 
 
     HeapSort(arr, n);
     
-    printHeap(arr, n);  //3 5 8 9 10 11 13 14 18 70 
+    printHeap(arr, n);  // 3 5 8 9 10 11 13 14 18 70 
     
     return 0;
 }
