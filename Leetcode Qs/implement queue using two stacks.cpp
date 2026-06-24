69040d1be7c32c51a3f6b677a93d2262e6d8a125
Anee006 <aneesha.sibyj@gmail.com>
2026-06-24 19:45:03 +0530

    minor fixes

--- Leetcode Qs/implement queue using two stacks.cpp
+++ Leetcode Qs/implement queue using two stacks.cpp
@@ -1,7 +1,7 @@
-//IMPLEMENT QUEUE USING TWO STACKS
+// Leetcode 232
 
-//Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue 
-//(push, peek, pop, and empty).
+// Implement a first in first out (FIFO) queue using only two stacks. 
+// The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
 
 /* 
 Input:
@@ -19,82 +19,76 @@ myQueue.pop(); // return 1, queue is [2]
 myQueue.empty(); // return false
 */
 
-//LOGIC:
-//create two stacks s1 and s2. s1 is the primary stack and s2 is the helper.
-//map the top element of s1 so that it becomes the front el of the queue.
-//so front() is, front = s1.top()
+// LOGIC:
+// create two stacks s1 and s2. s1 is the primary stack and s2 is the helper.
+// map the top element of s1 so that it becomes the front el of the queue.
+// so front() is, front = s1.top()
 
-//STEPS TO IMPLEMENT push(int x):
-//copy all elements of s1 to s2.
-//s1.push(x)
-//copy all elements of s2 back to s1.
+// STEPS TO IMPLEMENT push(int x):
+// copy all elements of s1 to s2.
+// s1.push(x)
+// copy all elements of s2 back to s1.
 
 #include <iostream>
 #include <stack>
 using namespace std;
 
-class MyQueue
-{
+class MyQueue {
 public:
     stack <int> s1;
     stack <int> s2;
 
-    MyQueue()
-    {
+    MyQueue() {
 
     }
 
-    //Pushes element x to the back of the queue. //TC = O(n)
-    void push(int x)
-    {
-        //copy elements of s1 to s2:
-        while(!s1.empty())
-        {
+    // Pushes element x to the back of the queue. 
+    void push(int x) {
+        // copy elements of s1 to s2:
+        while(!s1.empty()) {
             s2.push(s1.top());
             s1.pop();
         }
 
-        //insert data in s1
+        // insert data in s1
         s1.push(x);
 
-        //copy elements from s2 to s1:
-        while(!s2.empty())
-        {
+        // copy elements from s2 to s1:
+        while(!s2.empty()) {
             s1.push(s2.top());
             s2.pop();
         }
     }
+    // TC = O(n)
 
-    //Removes the element from the front of the queue and returns it. //TC = O(1)
-    int pop()
-    {
-        int ans = s1.top(); //to preserve the val of top
+    // Removes the element from the front of the queue and returns it. 
+    int pop() {
+        int ans = s1.top(); // to preserve the val of top
         s1.pop();
         return ans;
     }
+    // TC = O(1)
 
-    //Returns the element at the front of the queue. //TC = O(1)
-    int peek()
-    {
+    // Returns the element at the front of the queue. 
+    int peek() {
         return s1.top();
     }
+    // TC = O(1)
 
-    //Returns true if the queue is empty, false otherwise. //TC = O(1)
-    bool empty()
-    {
+    // Returns true if the queue is empty, false otherwise. 
+    bool empty() {
         return s1.empty();
     }
+    // TC = O(1)
 };
 
-int main()
-{
+int main() {
     MyQueue* q = new MyQueue();
-    q->push(1); //is front of the queue
+    q->push(1); // is front of the queue
     q->push(2); 
     cout << q->peek() << endl;
     cout << q->pop() << endl;
     cout << q->empty() << endl;
-
 
     return 0;
 }
