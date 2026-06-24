8ed0d2329efafbbf173590db3c9309d26212ec88
Anee006 <aneesha.sibyj@gmail.com>
2026-06-24 15:37:20 +0530

    Added TC

--- Binary search tree/search in BST.cpp
+++ Binary search tree/search in BST.cpp
@@ -1,7 +1,7 @@
-//SEARCH IN A BST (LeetCode 700)
+// LeetCode 700
 
-//You are given the root of a binary search tree (BST) and an integer val.
-//Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If node does not exist, return null.
+// You are given the root of a binary search tree (BST) and an integer val.
+// Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If node does not exist, return null.
 
 #include <iostream>
 #include <vector>
@@ -21,78 +21,67 @@ class Node
         }
 };
 
-Node* insert(Node* root, int val)
-{
+Node* insert(Node* root, int val) {
     if(root == NULL) return new Node(val);
 
-    if(val < root->data)
-    {
+    if(val < root->data) {
         root->left = insert(root->left, val);
     }
 
-    else
-    {
+    else {
         root->right = insert(root->right, val);
     }
 
     return root;
 }
 
-Node* buildBST(vector<int> arr)
-{
+Node* buildBST(vector<int> arr) {
     Node* root = NULL;
 
-    for(int val: arr)
-    {
-        root = insert(root, val);
-    }
+    for(int val: arr) root = insert(root, val);
+    
     return root;
 }
 
-Node* searchBST(Node* root, int key)
-{
+Node* searchBST(Node* root, int key) {
     if(root == NULL) return NULL;
 
-    if(root->data == key) return root; //means key exists so return that node
+    if(root->data == key) return root; // means key exists so return that node
 
-    //if not found, search in lst and rst
-    if(key < root->data)
-    {
+    // if not found, search in lst and rst
+    if(key < root->data) {
         return searchBST(root->left, key);
     }
-    else
-    {
+    else {
         return searchBST(root->right, key);
     }
 }
 
-int main()
-{
+int main() {
     vector <int> arr = {4,2,1,7,3};
 
     Node* root = buildBST(arr);
 
-    Node* node = searchBST(root, 2); //2 exists in BST
-    if(node != NULL)
-    {
+    Node* node = searchBST(root, 2); // 2 exists in BST
+
+    if(node != NULL) {
         cout << node->data << endl;
     }
-    else
-    {
+    else {
         cout << "2 does not exist in the given BST" << endl;
     }
-
     cout << endl;
 
-    Node* x = searchBST(root, -1); //-1 does not exist in BST
-    if(x != NULL)
-    {
+    Node* x = searchBST(root, -1); // -1 does not exist in BST
+    if(x != NULL) {
         cout << x->data << endl;
     }
-    else
-    {
+    else {
         cout << "-1 does not exist in this BST" << endl;
     }
 
     return 0;
 }
+
+// TC = O(n*n) --> for skewed BST
+// TC = O(n log n) --> for balanced BST
