4ff0a7ec0dbadde8223231ccde39a796a3bf27ef
Anee006 <aneesha.sibyj@gmail.com>
2026-06-24 21:17:36 +0530

    added TC and SC

--- Leetcode Qs/pair sum_1.cpp
+++ Leetcode Qs/pair sum_1.cpp
@@ -1,40 +1,37 @@
-//PAIR SUM using BRUTE FORCE APPROACH
-//For the given array ={2,7,11,15}, return the pair with the target sum of 9.
+// BRUTE FORCE APPROACH
+// For the given array = {2,7,11,15}, return the pair with the target sum of 9.
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
-vector <int> pairSum(vector <int> nums, int target) //fn to get the indices of the req pair
-{
-    vector <int> ans;
-    int n=nums.size();
+// to get the indices of the req pair
+vector<int> pairSum(vector<int> nums, int target) {
+    vector<int> ans;
+    int n = nums.size();
 
-    for(int i=0;i<n;i++)
-    {
-        for(int j=i+1; j<n; j++)
-        {
-            if(nums[i]+ nums[j] == target)
-            {
+    for(int i=0; i<n; i++) {
+        for(int j = i+1; j<n; j++) {
+            if(nums[i]+ nums[j] == target) {
                 ans.push_back(i);
                 ans.push_back(j);
-                return ans;  //ans is returned in the form of vector
+                return ans;  
             }
         }
     }
-
-    return ans; //will return empty vector if ans doesn't exist
-
+    return ans; // will return empty vector if ans doesn't exist
 }
 
-int main()
-{
-    vector <int> nums={2,7,11,15};
-    int target=9;
-    int n=nums.size();
+int main() {
+    vector<int> nums = {2,7,11,15};
+    int target = 9;
+    int n = nums.size();
 
-    vector <int> ans= pairSum(nums,target);
-    cout<<ans[0]<<" , "<<ans[1]<<endl;
+    vector<int> ans = pairSum(nums,target);
+    cout << ans[0] << " , " << ans[1] << endl;
 
     return 0;
 }
+
+// TC = O(n^2)
+// SC = O(1)
