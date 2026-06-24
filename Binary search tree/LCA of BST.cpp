4914d76475518279c90ecde6d4c7849dd22b47cb
Anee006 <aneesha.sibyj@gmail.com>
2026-06-24 14:32:57 +0530

    added TC

--- Binary search tree/LCA of BST.cpp
+++ Binary search tree/LCA of BST.cpp
@@ -1,21 +1,22 @@
 // #newnotes
-//LOWEST COMMON ANCESTOR OF BST (LeetCode 235)
+// LeetCode 235
+// LOWEST COMMON ANCESTOR OF BST 
 
-//Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
-//The lowest common ancestor is defined between two nodes p and q as the lowest node in tree that has both p and q as descendants 
-//(where we allow a node to be a descendant of itself).”
+// Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+// The lowest common ancestor is defined between two nodes p and q as the lowest node in tree that has both p and q as descendants 
+// (where we allow a node to be a descendant of itself).”
 
-//Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
-//Output: 6
+// Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+// Output: 6
 
 
-//LCA --> is the first node in whose lst and rst both p and q exist.
+// LCA --> is the first node in whose lst and rst both p and q exist.
 
-//to find p and q we will use BST properties.
-//case 1: if(root->val > p->val && root->val > q->val) --> ans will be in the LST
-//case 2: else if(root->val < p->val && root->val < q->val) --> ans will be in the RST
-//case 3: p exists in the LST and q exists in the RST --> this is the condition for an LCA  --> return root as the ans
-//case 4: when either of p or q is the LCA --> is handled in case 3 itself --> root will be the ans in this case as well
+// to find p and q we will use BST properties.
+// case 1: if(root->val > p->val && root->val > q->val) --> ans will be in the LST
+// case 2: else if(root->val < p->val && root->val < q->val) --> ans will be in the RST
+// case 3: p exists in the LST and q exists in the RST --> this is the condition for an LCA  --> return root as the ans
+// case 4: when either of p or q is the LCA --> is handled in case 3 itself --> root will be the ans in this case as well
 
 #include <iostream>
 using namespace std;
@@ -34,33 +35,24 @@ class Node
         }
 };
 
-Node* LCA(Node* root, Node* p, Node* q)
-{
-    if(root == NULL) return NULL; //base case
+Node* LCA(Node* root, Node* p, Node* q) {
+    if(root == NULL) return NULL; // base case
 
     // case 1:
-    if(root->data > p->data && root->data > q->data)  //means p & q are both less than root --> ans is in LST
-    {
+    if(root->data > p->data && root->data > q->data) { // means p & q are both less than root --> ans is in LST
         return LCA(root->left, p, q);
     }
 
-    //case 2:
-    else if(root->data < p->data && root->data < q->data) //means p & q are both greater than root --> ans is in RST
-    {
+    // case 2:
+    else if(root->data < p->data && root->data < q->data) { // means p & q are both greater than root --> ans is in RST
         return LCA (root->right, p, q);
     }
 
-    //case 3:
-    else
-    {
-        return root; 
-    }
+    // case 3:
+    else return root; 
 }
-//TC = O(height of tree)
-// Since, it is a balanced BST, TC = O(log n).
 
-int main()
-{
+int main() {
     Node* root = new Node(6);
     root->left = new Node(2);
     root->right = new Node(8);
@@ -71,7 +63,7 @@ int main()
     root->left->right->left = new Node(3);
     root->left->right->right = new Node(5);
 
-    Node* p1 = root->left->right->left; //p = 3
+    Node* p1 = root->left->right->left; // p = 3
     Node* q1 = root->left->right->right;  // q = 5
     Node* lca_1= LCA(root, p1, q1);
 
@@ -84,3 +76,6 @@ int main()
     cout << "LCA of " << p2->data << " and " << q2->data << " is: " << lca_2->data << endl;
     return 0;
 }
+
+// TC = O(height of tree)
+// Since, it is a balanced BST, TC = O(log n).
