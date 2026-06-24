80571d6f22be8e68fb82b7d333ae09218dd670ff
Anee006 <aneesha.sibyj@gmail.com>
2026-06-24 16:33:01 +0530

    minor fixes

--- DP/Bellman Ford algo.cpp
+++ DP/Bellman Ford algo.cpp
@@ -9,23 +9,19 @@
 #include <queue>
 using namespace std;
 
-class Edge
-{
+class Edge {
 public:
     int v;
     int w;
 
-    Edge(int v, int w)
-    {
+    Edge(int v, int w) {
         this->v = v;
         this->w = w;
     }
 };
 
-void printPath(int v, vector<int>& parent)
-{
-    if(parent[v] == -1) // src node
-    {
+void printPath(int v, vector<int>& parent) {
+    if(parent[v] == -1) { // src node
         cout << v;
         return;
     }
@@ -33,22 +29,17 @@ void printPath(int v, vector<int>& parent)
     cout << " -> " << v;
 }
 
-void bellmanFord(int src, vector<vector<Edge>> g, int V)
-{
+void bellmanFord(int src, vector<vector<Edge>> g, int V) {
     vector<int> dis(V, INT_MAX); // to store shortest dis from src to each node
     dis[src] = 0;
 
     vector<int> parent(V, -1);
 
     // do edge relaxation for V-1 times
-    for(int i=0; i< V-1; i++)
-    {
-        for(int u=0; u<V; u++) // for an edge:  u ---> v
-        {
-            for(Edge e: g[u])
-            {
-                if(dis[u] != INT_MAX && dis[u] + e.w < dis[e.v])
-                {
+    for(int i = 0; i < V-1; i++) {
+        for(int u = 0; u < V; u++) { // for an edge:  u ---> v
+            for(Edge e: g[u]) {
+                if(dis[u] != INT_MAX && dis[u] + e.w < dis[e.v]) {
                     dis[e.v] = dis[u] + e.w;
                     parent[e.v] = u;
                 }
@@ -57,12 +48,9 @@ void bellmanFord(int src, vector<vector<Edge>> g, int V)
     }
 
     // to detect negative weight cycles (if exists, Bellman Ford algo fails and needs to stop)
-    for(int u=0; u<V; u++)
-    {
-        for(Edge e: g[u])
-        {
-            if(dis[u] != INT_MAX && dis[u] + e.w < dis[e.v])
-            {
+    for(int u = 0; u < V; u++) {
+        for(Edge e: g[u]) {
+            if(dis[u] != INT_MAX && dis[u] + e.w < dis[e.v]) {
                 cout << "Negative weight cycle detected! Cannot proceed further.." << endl;
                 return;
             }
@@ -70,8 +58,7 @@ void bellmanFord(int src, vector<vector<Edge>> g, int V)
         }
     }
 
-    for(int i=0; i<V; i++)
-    {
+    for(int i=0; i<V; i++) {
         cout << "Shortest distance to " << i << " = " << dis[i] << " , Path: ";
         printPath(i, parent);
         cout << endl;
@@ -79,8 +66,7 @@ void bellmanFord(int src, vector<vector<Edge>> g, int V)
     cout << endl;
 }
 
-int main()
-{
+int main() {
     int V = 5; // 5 vertices
     vector<vector<Edge>> g(V);
 
