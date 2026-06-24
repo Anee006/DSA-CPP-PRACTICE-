d6efdb70877735cfbf82bd9ec26cc944d1b0cb56
Anee006 <aneesha.sibyj@gmail.com>
2026-06-24 20:16:20 +0530

    added TC and SC

--- Leetcode Qs/max row sum in a matrix.cpp
+++ Leetcode Qs/max row sum in a matrix.cpp
@@ -1,39 +1,37 @@
-//MAXIMUM ROW SUM
+// MAXIMUM ROW SUM
 
-//If we have a 2d matrix: {{1,2,3}, {4,5,6}, {7,8,9}}.
-//Sum of elements of 1st row= 1+2+3 = 6
-//Sum of elements of 2nd row= 4+5+6 = 15
-//Sum of elements of 3rd row= 7+8+9 = 24
-//max of the 3 sums is 24, so the ANS IS 24.
+// If we have a 2d matrix: {{1,2,3}, {4,5,6}, {7,8,9}}.
+// Sum of elements of 1st row= 1+2+3 = 6
+// Sum of elements of 2nd row= 4+5+6 = 15
+// Sum of elements of 3rd row= 7+8+9 = 24
+// max of the 3 sums is 24, so the ANS IS 24.
 
 #include <iostream>
 using namespace std;
 
-int maxRowSum(int matrix[3][3], int rows, int cols)
-{
-    int MaxRowSum= INT_MIN;
+int maxRowSum(int matrix[3][3], int rows, int cols) {
+    int MaxRowSum = INT_MIN;
 
-    for(int i=0; i<rows; i++)
-    {
-        int rowSum=0;
-        for(int j=0; j<cols; j++)
-        {
-            rowSum += matrix[i][j]; //to calc sum of each row (i.e, of the i'th row)
+    for(int i = 0; i < rows; i++) {
+        int rowSum = 0;
+        for(int j = 0; j < cols; j++) {
+            rowSum += matrix[i][j]; // to calc sum of each row (i.e, of the i'th row)
         }
 
-        MaxRowSum= max(MaxRowSum, rowSum); //the max of the two will be stored, which will be the ans
+        MaxRowSum = max(MaxRowSum, rowSum); // the max of the two will be stored, which will be the ans
     }
-    return MaxRowSum; //req max sum
+    return MaxRowSum; 
 }
 
+int main() {
+    int matrix[3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
+    int rows = 3;
+    int cols = 3;
 
-int main()
-{
-    int matrix[3][3]= {{1,2,3}, {4,5,6}, {7,8,9}};
-    int rows=3;
-    int cols=3;
-
-    cout<< maxRowSum(matrix,rows,cols)<<endl;
+    cout << maxRowSum(matrix,rows,cols) << endl;
 
     return 0;
 }
+
+// TC = O(n x m) , where n = no. of rows, m = no. of cols
+// SC = O(1)
