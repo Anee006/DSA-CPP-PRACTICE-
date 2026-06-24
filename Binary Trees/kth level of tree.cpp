a99332f00fc7673b44cb044f407bda8e80159952
Anee006 <aneesha.sibyj@gmail.com>
2026-06-24 15:56:40 +0530

    added TC

--- Binary Trees/kth level of tree.cpp
+++ Binary Trees/kth level of tree.cpp
@@ -1,21 +1,19 @@
 // #newnotes
-//Kth LEVEL OF A BINARY TREE
+// Kth LEVEL OF A BINARY TREE
 
-//Given the root of a binary tree and "k". Print all those nodes which occur at the Kth level of the binary tree.
+// Given the root of a binary tree and "k". Print all those nodes which occur at the Kth level of the binary tree.
 
-//For the given binary tree : , k = 3
-//So the nodes that occur at level 3 are: 7, 4, 5
+// For the given binary tree : , k = 3
+// So the nodes that occur at level 3 are: 7, 4, 5
 
-//LOGIC:
-//we can solve this using either levelOrder (i.e iterative approach) or we can use recursive approach.
+// LOGIC:
+// we can solve this using either levelOrder (i.e iterative approach) or we can use recursive approach.
 
-//To solve using recursion:
-//1. At the root level, k = 3.
-//2. for the left & right child of the root, k = 3-1 = 2  (we will decrease k value at each level for left & right child by 1).
-//3. For the next level also, k = 2-1 = 1
-//4. When k becomes 1, we have to print the nodes at that level, as that is the level which we was originally asked to print.
-
-//TC = O(n)
+// To solve using recursion:
+// 1. At the root level, k = 3.
+// 2. for the left & right child of the root, k = 3-1 = 2  (we will decrease k value at each level for left & right child by 1).
+// 3. For the next level also, k = 2-1 = 1
+// 4. When k becomes 1, we have to print the nodes at that level, as that is the level which we was originally asked to print.
 
 #include <iostream>
 #include <vector>
@@ -36,15 +34,12 @@ class Node
 };
 
 static int idx = -1;
-Node* buildTree(vector<int> preorder)
-{
+
+Node* buildTree(vector<int> preorder) {
     idx++;
 
-    if(preorder[idx] == -1)
-    {
-        return NULL;
-    }
-
+    if(preorder[idx] == -1) return NULL;
+    
     Node* root = new Node(preorder[idx]);
 
     root->left = buildTree(preorder);
@@ -53,29 +48,25 @@ Node* buildTree(vector<int> preorder)
     return root;
 }
 
-void KthLevel(Node* root, int k)
-{
-    if(root == NULL)  //if no valid left and right child of root exists, we simply return back
-    {
-        return;
-    }
+void KthLevel(Node* root, int k) {
+    if(root == NULL) return; // if no valid left and right child of root exists, we simply return back
 
-    if(k == 1)  //means we have reached the desired level we wanted to print
-    {
+    if(k == 1) { // means we have reached the desired level we wanted to print
         cout << root->data << " ";
         return;
     }
 
-    //if not found the level, go to left and right child
-    KthLevel(root->left , k-1);  //we reduce the value of k for each level by 1
+    // if not found the level, go to left and right child
+    KthLevel(root->left , k-1);  // we reduce the value of k for each level by 1
     KthLevel(root->right, k-1);
 }
 
-int main()
-{
+int main() {
     vector <int> preorder = {1,2,7,-1,-1,-1,3,4,-1,-1,5,-1,-1};
 
     Node* root = buildTree(preorder);
 
-    KthLevel(root, 3); //here k = 3
+    KthLevel(root, 3); // here k = 3
 }
+
+// TC = O(n)
