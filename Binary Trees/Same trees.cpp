868c9b16786d3b9bfc21bd41cd9ac74c351aea4d
Anee006 <aneesha.sibyj@gmail.com>
2026-06-24 16:13:10 +0530

    minor fixes

--- Binary Trees/Same trees.cpp
+++ Binary Trees/Same trees.cpp
@@ -1,14 +1,20 @@
-//SAME TREE (LeetCode 100)
-//NOTE: NOT the same as isMirror Q
+// LeetCode 100
+// NOTE: NOT the same as isMirror Q
 
-//Given the roots of two binary trees p and q, write a function to check if they are the same or not.
-//NOTE: Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+// Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+// NOTE: Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
 
-//Input: p = [1,2,3], q = [1,2,3]
-//Output: true
+// Input: p = [1,2,3], q = [1,2,3]
+// Output: true
 
-//Input: p = [1,2], q = [1,null,2]
-//Output: false
+// Input: p = [1,2], q = [1,null,2]
+// Output: false
+
+// STEPS:
+// 1. check if left subtree is same for p and q. (i.e, isLeftSame)
+// 2. check if right subtree is same for p and q. (i.e, isRightSame)
+// 3. Check if roots p and q are identical.
+// tree is identical only if isLeftSame && isRightSame && p->data == q->data.
 
 #include <iostream>
 #include <vector>
@@ -28,33 +34,24 @@ class Node
         }
 };
 
-//STEPS:
-//1. check if left subtree is same for p and q. (i.e, isLeftSame)
-//2. check if right subtree is same for p and q. (i.e, isRightSame)
-//3. Check if roots p and q are identical.
-//tree is identical only if isLeftSame && isRightSame && p->data == q->data.
-
-bool isSame(Node* p, Node* q)
-{
-    //base case:
-    if(p == NULL || q == NULL)
-    {
-        return p == q;  //here, if both p and q are NULL then returns true else if any one of p and q is NULL, returns false.
+bool isSame(Node* p, Node* q) {
+    // base case:
+    if(p == NULL || q == NULL) {
+        return p == q;  // here, if both p and q are NULL then returns true else if any one of p and q is NULL, returns false.
     }
 
-    //check if left subtree is same
+    // check if left subtree is same
     bool isLeftSame = isSame(p->left, q->left);
 
-    //check if right subtree is same
+    // check if right subtree is same
     bool isRightSame = isSame(p->right, q->right);
 
-    //check if trees are identical
-    return isLeftSame && isRightSame && p->data == q->data; //all three conditions need to be true for both trees to be identical
+    // check if trees are identical
+    return isLeftSame && isRightSame && p->data == q->data; // all three conditions need to be true for both trees to be identical
 }
 
-int main()
-{
-    //for identical
+int main() {
+    // for identical
     Node* p = new Node(1);
     p->left = new Node(2);
     p->right = new Node(3);
@@ -63,24 +60,21 @@ int main()
     q->left = new Node(2);
     q->right = new Node(3);
 
-    if(isSame(p,q)) 
-        cout << "Both trees are identical\n";
-    else   
-        cout << "Both trees are not identical\n";
+    if(isSame(p,q)) cout << "Both trees are identical\n";
+
+    else cout << "Both trees are not identical\n";
     
-    //not identical:
+    // not identical:
     Node* root1 = new Node(1);
     root1->left = new Node(2);
 
     Node* root2 = new Node(1);
     root2->right = new Node(2);
 
-    if(isSame(root1,root2)) 
-        cout << "Both trees are identical\n";
-    else   
-        cout << "Both trees are not identical\n";
+    if(isSame(root1,root2)) cout << "Both trees are identical\n";
+    else cout << "Both trees are not identical\n";
 
     return 0;
 }
 
-//TC = O(n), where n is size of tree (where both trees are identical)
+// TC = O(n), where n is size of tree (where both trees are identical)
