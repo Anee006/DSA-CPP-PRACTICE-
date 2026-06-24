1c2976641ddbd52253fa4187f22d9087f21ebb16
Anee006 <aneesha.sibyj@gmail.com>
2026-06-24 18:22:53 +0530

    minor fixes

--- Graphs/Course schedule.cpp
+++ Graphs/Course schedule.cpp
@@ -1,5 +1,5 @@
 // #newnotes
-// COURSE SCHEDULE (LeetCode 207)
+// LeetCode 207
 // (concept is of: topological sort)
 // We will use "cycle detection in directed graph using DFS" to solve this Q.
 
@@ -24,63 +24,43 @@
 using namespace std;
 
 // to detect if a cycle is present in the graph, using DFS
-bool isCycleDFS(int src, vector<bool>& vis, vector<bool>& recPath, vector<vector<int>>& prerequisites)
-{
+bool isCycleDFS(int src, vector<bool>& vis, vector<bool>& recPath, vector<vector<int>>& prerequisites) {
     vis[src] = true;
     recPath[src] = true; // storing src node in the recursion path
 
-    for(int i=0; i<prerequisites.size(); i++) // loop on all "edges" of the graph
-    {
-        // find all "u" & "v" vertex for an edge: u----->v
+    for(int i = 0; i < prerequisites.size(); i++) { // loop on all "edges" of the graph
+        // find all "u" & "v" vertex for an edge: u -----> v
         // eg, for [1,0] , "u" = 0 and "v" = 1.
 
         int v = prerequisites[i][0]; // the "first" val of the pair (say [1,0]) will be "v"
         int u = prerequisites[i][1]; // the "second" val of the pair (say [1,0]) will be "u"
 
         // find all neighbors of "src"
-        if(u == src) // means "v" will be the neighbor of "u"
-        {
-            if(!vis[v])
-            {
-                if(isCycleDFS(v, vis, recPath, prerequisites))
-                {
-                    return true;
-                }
+        if(u == src) { // means "v" will be the neighbor of "u"
+            if(!vis[v]) {
+                if(isCycleDFS(v, vis, recPath, prerequisites)) return true;
             }
 
-            else if(recPath[v]) // if neighbor is already visited and is also part of the rec path
-            {
-                return true;
-            }
+            else if(recPath[v]) return true; // if neighbor is already visited and is also part of the rec path
         }
     }
     recPath[src] = false; // backtracking
     return false;
 }
 
-bool canFinish(int numCourses, vector<vector<int>>& prerequisites)
-{
+bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
     vector<bool> vis(numCourses, false); // size of vector = no. of courses we have (vertices)
     vector<bool> recPath(numCourses, false);
 
-    for(int i=0; i<numCourses; i++)
-    {
-        if(!vis[i])
-        {
-            if(isCycleDFS(i, vis, recPath, prerequisites)) // means cycle exists --> not a DAG --> return false
-            {
-                return false;
-            }
+    for(int i = 0; i < numCourses; i++) {
+        if(!vis[i]) {
+            if(isCycleDFS(i, vis, recPath, prerequisites)) return false; // means cycle exists --> not a DAG --> return false
         }
     }
     return true;
 }
 
-// TC = O(V+E), where V = no. of vertices and E = no. of edges
-// SC = O(V) 
-
-int main()
-{
+int main() {
     int numCourses = 4; // no. of courses = 4
 
     vector<vector<int>> prerequisites = {
@@ -90,10 +70,7 @@ int main()
         {3,2}
     };
 
-    if(canFinish(numCourses, prerequisites))
-    {
-        cout << "Can finish courses\n"; // returns this
-    }
+    if(canFinish(numCourses, prerequisites)) cout << "Can finish courses\n"; // returns this
 
     else cout << "Cannot finish courses\n";
 
@@ -106,13 +83,12 @@ int main()
         {1,0},
     };
 
-    if(canFinish(numCourses, prerequisites_2))
-    {
-        cout << "Can finish courses\n";
-    }
+    if(canFinish(numCourses, prerequisites_2)) cout << "Can finish courses\n";
 
     else cout << "Cannot finish courses\n"; // returns this
 
-
     return 0;
 }
+
+// TC = O(V+E), where V = no. of vertices and E = no. of edges
+// SC = O(V) 
