27692f2455bd759c8c286b7362fa69da0462a631
Anee006 <aneesha.sibyj@gmail.com>
2026-06-25 16:53:59 +0530

    minor fixes

--- Recursion (Backtracking)/subsets.cpp
+++ Recursion (Backtracking)/subsets.cpp
@@ -1,41 +1,34 @@
-//SUBSETS
-//using backtracking
+// Leetcode 78
 
-//Given an integer array nums of unique elements, return all possible subsets (the power set).
-//The solution set must not contain duplicate subsets. Return the solution in any order.
+// Given an integer array nums of unique elements, return all possible subsets (the power set).
+// The solution set must not contain duplicate subsets. Return the solution in any order.
 
-//Input: nums = [1,2,3]
-//Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+// Input: nums = [1,2,3]
+// Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
-//helper fn to calculate all subsets:
-void getAllSubsets(vector<int>& nums, vector<int>& ans, int i, vector<vector<int>>& allSubsets)
-{
-    //base case:
-    if( i == nums.size())
-    {
-        allSubsets.push_back({ans}); //the ans will be pushed back as a vector due to {}
+// helper fn to calculate all subsets:
+void getAllSubsets(vector<int>& nums, vector<int>& ans, int i, vector<vector<int>>& allSubsets) {
+    // base case:
+    if(i == nums.size()) {
+        allSubsets.push_back({ans}); // the ans will be pushed back as a vector due to {}
         return;
     }
 
-    //to include element:
+    // to include element:
     ans.push_back(nums[i]);
     getAllSubsets(nums, ans, i+1, allSubsets);
 
-    ans.pop_back(); //backtracking
+    ans.pop_back(); // backtracking
 
-    //to exclude element:
+    // to exclude element:
     getAllSubsets(nums, ans, i+1, allSubsets);
-
 }
 
-
-//Given in Q:
-vector<vector<int>> subsets(vector<int>& nums)
-{
+vector<vector<int>> subsets(vector<int>& nums) {
     vector<int> ans;
     vector<vector<int>> allSubsets;
 
@@ -43,21 +36,16 @@ vector<vector<int>> subsets(vector<int>& nums)
     return allSubsets;
 }
 
-int main()
-{
+int main() {
     vector<int> nums = {1,2,3};
     vector<vector<int>> ans = subsets(nums);
 
-    for(int i=0; i<ans.size(); i++)
-    {
-        for(int j=0; j<ans[i].size(); j++)
-        {
-            cout << ans[i][j] << " ";
-        }
+    for(int i = 0; i < ans.size(); i++) {
+        for(int j = 0; j < ans[i].size(); j++) cout << ans[i][j] << " ";
         cout << endl;
     }
     return 0;
 }
 
-//TC = O(n * 2^n) or can also write as: O(2^n), as there are 2^n subsets possible for n elements.
-//SC= O(n)
+// TC = O(n * 2^n) or can also write as: O(2^n), as there are 2^n subsets possible for n elements.
+// SC = O(n)
