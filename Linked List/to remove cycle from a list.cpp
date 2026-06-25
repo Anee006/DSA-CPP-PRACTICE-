4b26fc151804a441c4418a8a455eacc0c1d13913
Anee006 <aneesha.sibyj@gmail.com>
2026-06-25 15:42:10 +0530

    minor fixes

--- Linked List/to remove cycle from a list.cpp
+++ Linked List/to remove cycle from a list.cpp
@@ -1,73 +1,63 @@
-//To remove cycle from a linked list
-//using slow-fast pointer approach
+// To remove cycle from a linked list
+// using slow-fast pointer approach
 
-//There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
-//Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. 
-//Note that pos is not passed as a parameter.
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
+// Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. 
+// Note that pos is not passed as a parameter.
 
-//Input: head = [3,2,0,-4], pos = 1
+// Input: head = [3,2,0,-4], pos = 1
 
-//Input: head = [1], pos = -1
-
-//TC = O(n)
-//SC = O(1)
+// Input: head = [1], pos = -1
  
 #include <iostream>
 using namespace std;
 
 class Node
 {
-    public:
-        int data;
-        Node* next;
+public:
+    int data;
+    Node* next;
 
-        Node(int val)
-        {
-            data = val;
-            next = NULL;
-        }
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
 };
 
-//to print the list
-void printList(Node* head)
-{    
+// to print the list
+void printList(Node* head) {    
     Node* temp = head;
-    while (temp != NULL)
-    {
+
+    while (temp != NULL) {
         cout << temp->data << " -> ";
         temp = temp->next;
     }
     cout << "NULL" << endl;
 }
 
-//to remove cycle from the linked list
-Node* detectCycle(Node* head)
-{
-    Node* slow = head; //slow ptr
-    Node* fast = head; //fast ptr
+// to remove cycle from the linked list
+Node* detectCycle(Node* head) {
+    Node* slow = head; // slow ptr
+    Node* fast = head; // fast ptr
 
-    while (fast != NULL && fast->next != NULL)
-    {
+    while (fast != NULL && fast->next != NULL) {
         slow = slow->next;
-        fast = fast->next->next; //to detect if cycle is present
+        fast = fast->next->next; // to detect if cycle is present
 
-        if (slow == fast) //means cycle is present
-        {
-            slow = head; //to find start of the cycle
+        if (slow == fast) { // means cycle is present
+            slow = head; // to find start of the cycle
 
             // If the cycle starts at head
-            if (slow == fast)
-            {
-                while (fast->next != slow)
-                    fast = fast->next;
+            if (slow == fast) {
+                while (fast->next != slow) fast = fast->next;
 
                 fast->next = NULL; // Break the cycle
                 return slow;
             }
 
             // If cycle starts somewhere else
-            while (slow->next != fast->next)
-            {
+            while (slow->next != fast->next) {
                 slow = slow->next;
                 fast = fast->next;
             }
@@ -78,32 +68,29 @@ Node* detectCycle(Node* head)
     return NULL;
 }
 
-
-int main()
-{
-    //creating nodes:
+int main() {
+    // creating nodes:
     Node* head = new Node(3);
-    Node* second = new Node(2); //pos = 1
+    Node* second = new Node(2); // pos = 1
     Node* third = new Node(0);
     Node* fourth = new Node(-4);
 
-    //linking nodes normally:
+    // linking nodes normally:
     head->next = second;
     second->next = third;
     third->next = fourth;
 
-    //creating a cycle from fourth to second node (as given in Q)
+    // creating a cycle from fourth to second node (as given in Q)
     fourth ->next = second;
 
-    if (detectCycle(head))
-        cout << "Cycle detected and removed successfully." << endl;
+    if (detectCycle(head)) cout << "Cycle detected and removed successfully." << endl;
 
-    else
-        cout << "No cycle detected." << endl;
+    else cout << "No cycle detected." << endl;
         
     printList(head); // Should print linear list after cycle removal
 
-    
-    
     return 0;
 }
+
+// TC = O(n)
+// SC = O(1)
