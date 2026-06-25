941bf7fafb041cf2a5e71538650d905d6a1dc4f7
Anee006 <aneesha.sibyj@gmail.com>
2026-06-25 16:31:16 +0530

    Added TC

--- Recursion (Backtracking)/print string in rev order.cpp
+++ Recursion (Backtracking)/print string in rev order.cpp
@@ -1,25 +1,25 @@
-//to print a string in reverse order
+// to print a string in reverse order
 
 #include <iostream>
 #include <string>
 using namespace std;
 
-void print_reverse(string str, int idx) //str is the string to be reversed and idx is the variable to store the index.
-{
-    if(idx == 0) //base case
-    {
+void print_reverse(string str, int idx) { // str is the string to be reversed and idx is the variable to store the index.
+    if(idx == 0) { // base case
         cout << str.at(idx);
         return;
     }
 
-    cout << str.at(idx); //to print the character at that particular index
-    print_reverse(str, idx-1); //as we want to print in reverse order
+    cout << str.at(idx); // to print the character at that particular index
+    print_reverse(str, idx-1); // as we want to print in reverse order
 }
 
-int main()
-{
+int main() {
     string str = "abcd";
     print_reverse(str, str.length()-1);
     
     return 0;
 }
+
+// TC = O(n²)
+// SC = O(n²)
