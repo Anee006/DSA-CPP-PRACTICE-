e9bf7de608fa5b98a5db75cd076efa890ae5ed95
Anee006 <aneesha.sibyj@gmail.com>
2026-07-04 17:19:35 +0530

    min cost to connect all points using prims algo

--- Graphs/Min Cost to Connect All Points.cpp
+++ Graphs/Min Cost to Connect All Points.cpp
@@ -13,9 +13,10 @@ Output: 20
 
 // LOGIC:
 // There are 'n' points (vertices) and each point can make connection with (n-1) points.
+// Min cost to connect all points --> MST cost of graph.
+// Since, it is a dense graph (many edges can exist), use Prims algo to find MST.
 
-
-// Manhattan distance b/w all points:
+// Manhattan distance b/w all points of the given graph:
 //       P0      P1      P2      P3      P4
 //  P0   0       4       13      7       7
 //  P1   4       0       9       3       7
@@ -26,10 +27,44 @@ Output: 20
 
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
+// manhattan distance
+int manDist(vector<vector<int>>& points, int p1, int p2) {
+    return abs(points[p1][0] - points[p2][0]) + abs(points[p1][1] - points[p2][1]); //  |x1-x2| + |y1-y2|
+}
+
 int minCostConnectPoints(vector<vector<int>>& points) {
-        
+    int n = points.size();
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; // min heap
+
+    vector<bool> mstSet(n, false);
+    int mstCost = 0;
+
+    pq.push({0, 0}); // stores {weight, node}
+
+    while(!pq.empty()) {
+        auto p = pq.top();
+        int wt = p.first;
+        int node = p.second;
+
+        pq.pop();
+
+        if(mstSet[node]) continue; // we only need 1 edge till each node in MST
+
+        mstSet[node] = true;
+        mstCost += wt;
+
+        // visit unvisited neighbors
+        for(int i=0; i<n; i++) {
+            if(!mstSet[i]) {
+                int edgeWt = manDist(points, node, i);
+                pq.push({edgeWt, i});
+            }
+        }
+    }
+    return mstCost;
 }
 
 int main() {
@@ -39,3 +74,6 @@ int main() {
 
     return 0;
 }
+
+// TC = O(n^2  log n)
+// SC = O(n^2)
