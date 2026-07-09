dab7a639f704facc642ee46884dd170a3ca74d72
Anee006 <aneesha.sibyj@gmail.com>
2026-07-09 16:28:41 +0530

    Minor fix

--- Linked List/copy list with random pointer.cpp
+++ Linked List/copy list with random pointer.cpp
@@ -1,3 +1,4 @@
+// #notes
 // Leetcode 138
 // A linked list of length n is given such that each node contains an additional random pointer, which could point to any node in the list, or null.
 // Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes, where each new node has its value set to the value 
