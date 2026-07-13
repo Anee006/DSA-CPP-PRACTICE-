e8f441f2e580497bf9ccb1f0f79799de60e6e7b7
Anee006 <aneesha.sibyj@gmail.com>
2026-07-13 19:16:12 +0530

    sequential digits using enumeration

--- Leetcode Qs/sequential digits.cpp
+++ Leetcode Qs/sequential digits.cpp
@@ -11,8 +11,8 @@
 // (2 digit --> 8 nums, 3 digit --> 7 nums, 4 digit --> 6 nums... 9 digit --> 1 num) => 8 + 7 + 6 + 5 + 4 + 3 + 2 + 1 = 36
 
 // LOGIC:
-// Every sequential number is a substring of 123456789
-// For given eg, low = 1000, high = 13000. So, 1000 -> 4 digits , 13000 -> 5 digits
+// Every sequential number is a substring of "123456789"
+// For given e.g, low = 1000, high = 13000. So, 1000 -> 4 digits , 13000 -> 5 digits
 // For every length, generate every substring
 // All 4-digit numbers: 1234, 2345, 3456, 4567, 5678, 6789
 // All 5-digit numbers: 12345, 23456, 34567, 45678, 56789
@@ -23,7 +23,20 @@
 using namespace std;
 
 vector<int> sequentialDigits(int low, int high) {
+    string digits = "123456789";
+    vector<int> ans;
 
+    int minLen = to_string(low).size(); // number of digits in low
+    int maxLen = to_string(high).size(); // number of digits in high
+
+    for(int len = minLen; len <= maxLen; len++) {
+        for(int i = 0; i + len <= 9; i++) {
+            int num = stoi(digits.substr(i, len)); // string to int --> convert substring into int
+
+            if(num >= low && num <= high) ans.push_back(num);
+        }
+    }
+    return ans;
 }
 
 int main() {
