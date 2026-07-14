c7dc0d39f9fa3f5e4a990437e7dfc828c3825737
Anee006 <aneesha.sibyj@gmail.com>
2026-07-14 16:16:22 +0530

    daily temperatures

---
+++ Stack/daily temperatures.cpp
@@ -0,0 +1,36 @@
+// Leetcode 739
+// uses logic of "next greater element" Q
+
+// Given an array of integers 'temperatures' represents the daily temperatures, return an array answer such that answer[i] is the number of days you 
+// have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+/*
+Input: temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+Explanation:
+For day 0 -> 73 : next warmer (higher) temp is 74 --> which is 1 day later --> ans = 1
+For day 1 -> 74 : next warmer (higher) temp is 75 --> which is 1 day later --> ans = 1
+For day 2 -> 75 : next warmer (higher) temp is 76 --> which is 4 days later --> ans = 4
+For 76 : No higher temp exists so --> ans = 0
+
+Input: temperatures = [30,40,50,60]
+Output: [1,1,1,0]
+*/
+
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+vector<int> dailyTemperatures(vector<int>& temperatures) {
+        
+}
+
+int main() {
+    vector<int> temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+
+    vector<int> ans = dailyTemperatures(temperatures);
+    for(int i : ans) cout << i << " , ";
+
+    return 0;
+}
