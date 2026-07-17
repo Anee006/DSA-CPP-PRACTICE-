8fc83a64cbcd5defa2bea685ec6c182767854bf0
Anee006 <aneesha.sibyj@gmail.com>
2026-07-17 16:06:41 +0530

    max sum circular subarray using Kadane's algo

--- Array/maximum sum circular subarray.cpp
+++ Array/maximum sum circular subarray.cpp
@@ -1,4 +1,5 @@
 // Leetcode 918
+// Using Kadane's algo
 
 // Given a circular integer array nums of length n, return the maximum possible sum of a non-empty subarray of nums.
 // A circular array means the end of the array connects to the beginning of the array. Formally, the next element of nums[i] is nums[(i + 1) % n] 
