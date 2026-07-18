0d5d597589776734c150996cfd08ba19839155ae
Anee006 <aneesha.sibyj@gmail.com>
2026-07-18 11:32:02 +0530

    find GCD of array

--- Math/find greatest common divisor of array.cpp
+++ Math/find greatest common divisor of array.cpp
@@ -15,15 +15,22 @@ The greatest common divisor of 2 and 10 is 2.
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <numeric> // for gcd()
 using namespace std;
 
 int findGCD(vector<int>& nums) {
+    int smallest = *min_element(nums.begin(), nums.end()); // finds smallest element in the array
+    int largest = *max_element(nums.begin(), nums.end()); // finds largest element in the array
 
+    return gcd(smallest, largest);
 }
 
 int main() {
+    vector<int> nums = {2,5,6,9,10};
+    cout << findGCD(nums);
+
     return 0;
 }
 
-// TC = 
-// SC =
+// TC = O(n)
+// SC = O(1)
