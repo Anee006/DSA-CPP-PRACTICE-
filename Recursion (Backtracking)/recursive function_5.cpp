d9ad049eab9d3e9ae86712d9c827ea2cabcf4323
Anee006 <aneesha.sibyj@gmail.com>
2026-07-18 11:36:15 +0530

    minor fix

--- Recursion (Backtracking)/recursive function_5.cpp
+++ Recursion (Backtracking)/recursive function_5.cpp
@@ -5,7 +5,7 @@
 #include <string>
 using namespace std;
 
-void move_X(string str, int idx, int count, string newString) { // "count" tracks the no. of 'x' on the given string
+void move_X(string str, int idx, int count, string newString) { // "count" tracks the no. of 'x' in the given string
     if(idx == str.length()) { // base case
         for(int i = 0; i < count; i++) {
             newString = newString + 'x'; // adding x to the end of the string "count" no. of times
@@ -22,7 +22,7 @@ void move_X(string str, int idx, int count, string newString) { // "count" track
     }
     else {
         newString = newString + current; // or use +=
-        move_X(str,idx+1,count, newString);
+        move_X(str, idx+1, count, newString);
     }
 }
 
