86e302d525c89f22c853ab08e7beb1c06655924c
Anee006 <aneesha.sibyj@gmail.com>
2026-07-18 11:27:18 +0530

    find GCD of array

---
+++ Math/find greatest common divisor of array.cpp
@@ -0,0 +1,29 @@
+// Leetcode 1979
+
+// Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
+// The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+
+/*
+Input: nums = [2,5,6,9,10]
+Output: 2
+Explanation:
+The smallest number in nums is 2.
+The largest number in nums is 10.
+The greatest common divisor of 2 and 10 is 2.
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int findGCD(vector<int>& nums) {
+
+}
+
+int main() {
+    return 0;
+}
+
+// TC = 
+// SC =
