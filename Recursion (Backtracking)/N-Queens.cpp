b73a333e31a87732bc3e8f6eeaf610abdb3eb675
Anee006 <aneesha.sibyj@gmail.com>
2026-07-19 19:50:12 +0530

    minor fix

--- Recursion (Backtracking)/N-Queens.cpp
+++ Recursion (Backtracking)/N-Queens.cpp
@@ -1,5 +1,6 @@
 // #notes
 // Leetcode 51
+// Can also be used to solve Leetcode 52 (N-Queens II)
 
 // The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
 // Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
