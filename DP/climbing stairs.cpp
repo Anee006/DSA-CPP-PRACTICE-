c253ea7d361efb02a973c5f769950740709f83b1
Anee006 <aneesha.sibyj@gmail.com>
2026-07-25 18:41:22 +0530

    climbing stairs: solved using tabulation

--- DP/climbing stairs.cpp
+++ DP/climbing stairs.cpp
@@ -1,4 +1,6 @@
+// #newnotes
 // Leetcode 70
+// uses same logic as used for solving 1-D fibonacci using DP
 
 // You are climbing a staircase. It takes n steps to reach the top.
 // Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
@@ -13,10 +15,25 @@ Explanation: There are three ways to climb to the top.
 */
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
+// using tabulation + memory optimization
 int climbStairs(int n) {
-        
+    if(n == 1 || n == 2) return n; // base case
+
+    int prev2 = 1; // stores sol. of n = 1 , i.e (n-2)
+    int prev1 = 2; // stores sol. of n = 2 , i.e (n-1)
+
+    int result = prev1;
+
+    for(int i = 3; i <= n; i++) {
+        result = prev1 + prev2;
+
+        prev2 = prev1;
+        prev1 = result;
+    }
+    return result;
 }
 
 int main() {
@@ -26,3 +43,6 @@ int main() {
 
     return 0;
 }
+
+// TC = O(n)
+// SC = O(1)
