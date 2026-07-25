c41c8ae6e758f1431af1e476e61b83f8fcaae8b8
Anee006 <aneesha.sibyj@gmail.com>
2026-07-25 17:24:34 +0530

    climbing stairs

---
+++ DP/climbing stairs.cpp
@@ -0,0 +1,28 @@
+// Leetcode 70
+
+// You are climbing a staircase. It takes n steps to reach the top.
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+/*
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+*/
+
+#include <iostream>
+using namespace std;
+
+int climbStairs(int n) {
+        
+}
+
+int main() {
+    int n = 3;
+
+    cout << climbStairs(n);
+
+    return 0;
+}
