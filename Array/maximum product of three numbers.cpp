e0447748eb60d346ccb0be198e95e986e74b8ba8
Anee006 <aneesha.sibyj@gmail.com>
2026-07-26 10:59:34 +0530

    max product of 3 numbers

--- Array/maximum product of three numbers.cpp
+++ Array/maximum product of three numbers.cpp
@@ -22,9 +22,15 @@ Explanation: -10 * -10 * 5 = 500
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int maximumProduct(vector<int>& nums) {
+    int n = nums.size();
+
+    sort(nums.begin(), nums.end());
+
+    return max(nums[n-1] * nums[n-2] * nums[n-3] , nums[0] * nums[1] * nums[n-1]);
 }
 
 int main() {
