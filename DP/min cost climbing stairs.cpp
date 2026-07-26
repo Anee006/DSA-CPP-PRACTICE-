e47a0dc5f816a091779ee5148558bb4df21f070a
Anee006 <aneesha.sibyj@gmail.com>
2026-07-26 12:09:19 +0530

    min cost climbing stairs: solved using tabulation

--- DP/min cost climbing stairs.cpp
+++ DP/min cost climbing stairs.cpp
@@ -1,3 +1,4 @@
+// #newnotes
 // LeetCode 746
 
 // You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
@@ -14,7 +15,18 @@ Pay 15 and climb two steps to reach the top. The total cost is 15.
 #include <vector>
 using namespace std;
 
+// Using tabulation + space optimization
 int minCostClimbingStairs(vector<int>& cost) {
+    int n = cost.size();
+
+    int prev1 = 0; // acts as dp[0] -> dp[n-2]
+    int prev2 = 0; // acts as dp[1] -> dp[n-1]
+    int result = prev2;
+
+    for(int i = 2; i <= n; i++) {
+        result = min(prev2 + cost[i-1] , prev1 + cost[i-2]);
+    }
+    return result;
 }
 
 int main() {
@@ -24,3 +36,6 @@ int main() {
 
     return 0;
 }
+
+// TC = O(n)
+// SC = O(1)
