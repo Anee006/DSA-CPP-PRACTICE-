0407c65039a0b20162b8507730e7e1580bf3310d
Anee006 <aneesha.sibyj@gmail.com>
2026-07-27 13:13:40 +0530

    max product of two elements in an array

---
+++ Array/maximum product of two elements in an array.cpp
@@ -0,0 +1,33 @@
+// LeetCode 1464
+
+// Given the array of integers nums, you will choose two different indices i and j of that array. 
+// Return the maximum value of (nums[i]-1)*(nums[j]-1).
+
+/*
+Input: nums = [3,4,5,2]
+Output: 12 
+Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, 
+(nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12. 
+*/
+
+// LOGIC:
+// find the largest element: max1 and 2nd largest element max2.
+// Return (max1 - 1) * (max2 - 1)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxProduct(vector<int>& nums) {
+}
+
+int main() {
+    vector<int> nums = {3, 4, 5, 2};
+
+    cout << maxProduct(nums);
+
+    return 0;
+}
+
+// TC = O(n)
+// SC = O(1)
