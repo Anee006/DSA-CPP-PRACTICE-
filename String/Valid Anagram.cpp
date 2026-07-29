58e1b566df234470c622fb7a9850747ab2553154
Anee006 <aneesha.sibyj@gmail.com>
2026-07-29 22:05:08 +0530

    valid anagram: solved by counting frequencies and removing characters

--- String/Valid Anagram.cpp
+++ String/Valid Anagram.cpp
@@ -23,10 +23,31 @@ Character 't' is missing, and 'c' is extra
 "car" -> c = 1, a = 1, r = 1
 */
 
+// LOGIC:
+// Two strings are anagrams if they contain exactly the same characters with the same frequencies.
+// If the length of both strings are different, they can never be an anagram.
+// Create a freq array of size 26 (as only lowercase letters are there).
+// Traverse first string and increment frequency of each character.
+// Traverse second string and decrement frequency of each character.
+// Check every element of freq array. If, any value is not 0, return false. Else, return true.
+
 #include <iostream>
+#include <vector>
 using namespace std;
 
 bool isAnagram(string s, string t) {
+    if(s.size() != t.size()) return false;
+
+    vector<int> freq(26, 0); // each idx repr a letter. 'a' has idx = 0, 'b' has idx = 1 .... 'z' has idx = 25
+
+    for(char ch : s) freq[ch - 'a']++; // count char of 1st str
+    for(char ch : t) freq[ch - 'a']--; // remove char from 2nd str
+
+    for(int count : freq) {
+        if(count != 0) return false;
+    }
+
+    return true;
 }
 
 int main() {
