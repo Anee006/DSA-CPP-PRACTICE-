6e89915f3bba5f8dad35a392aa4db670c280e38d
Anee006 <aneesha.sibyj@gmail.com>
2026-07-30 21:27:30 +0530

    Min no. of pushed to type word I

--- String/Minimum Number of Pushes to Type Word I.cpp
+++ String/Minimum Number of Pushes to Type Word I.cpp
@@ -9,10 +9,23 @@
 // Return the minimum number of pushes needed to type word after remapping the keys.
 // Note that 1, *, #, and 0 do not map to any letters.
 
+// LOGIC:
+// Since all letters in word are distinct, each letter is typed exactly once.
+// Minimize the total push cost of assigning these letters to the 8 available keys (2–9).
+// The 1st letter assigned to a key costs 1 push. The 2nd letter assigned to the same key costs 2 pushes. The 3rd letter costs 3 pushes, and so on.
+// To minimize the total pushes: Put the first 8 letters in the first position of the 8 keys (cost = 1 each).
+// The next 8 letters go in the second position (cost = 2 each). The next 8 letters go in the third position (cost = 3 each).
+
 #include <iostream>
 using namespace std;
 
 int minimumPushes(string word) {
+    int ans = 0;
+
+    for(int i = 0; i < word.size(); i++) {
+        ans += i/8 + 1;
+    }
+    return ans;
 }
 
 int main() {
@@ -22,3 +35,6 @@ int main() {
 
     return 0;
 }
+
+// TC = O(n)
+// SC = O(1)
