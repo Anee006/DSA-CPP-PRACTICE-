a7a601955f44fdc52d8265db4749e399d593ab8f
Anee006 <aneesha.sibyj@gmail.com>
2026-08-01 11:47:45 +0530

    contains duplicate: solved using sorting

--- Array/contains duplicate_2.cpp
+++ Array/contains duplicate_2.cpp
@@ -18,6 +18,13 @@ Explanation: The element 1 occurs at the indices 0 and 3.
 using namespace std;
 
 bool containsDuplicate(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+
+    for(int i = 1; i < nums.size(); i++) {
+        if(nums[i] == nums[i-1]) return true;
+    }
+    
+    return false;
 }
 
 int main() {
