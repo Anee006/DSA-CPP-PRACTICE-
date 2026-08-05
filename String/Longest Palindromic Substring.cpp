5109914f085a83d84e52609589f6d4baf386097c
Anee006 <aneesha.sibyj@gmail.com>
2026-08-05 21:52:16 +0530

    longest palindromic substring: solved using expand around center

--- String/Longest Palindromic Substring.cpp
+++ String/Longest Palindromic Substring.cpp
@@ -12,12 +12,37 @@
 // for odd-length palindrome, center is at (i, i). For even length-palindrome, center is at (i, i+1).
 // for odd-length palindrome: left = right = i. For even-length palindrome: left = i, right = i+1.
 // take the longer of the two lengths. If it is longer than curr ans, update "start" and "end".
-// return substring from start to end
 
 #include <iostream>
 using namespace std;
 
+// expand around given center and return palindromic length
+int expand(string& s, int left, int right) {
+    while(left >= 0 && right < s.size() && s[left] == s[right]) {
+        left--;
+        right++;
+    }
+
+    return right - left - 1; // actual palindromic length (left and right have already moved one step outside the palindrome)
+}
+
 string longestPalindrome(string s) {
+    if(s.empty()) return "";
+
+    int start = 0, end = 0;
+
+    for(int i = 0; i < s.size(); i++) {
+        int len1 = expand(s, i , i); // odd-length palindrome
+        int len2 = expand(s, i , i+1); // even-length palindrome
+
+        int len = max(len1, len2);
+
+        if(len > end - start + 1) { // update ans if longer palindrome is found
+            start = i - (len - 1)/2;
+            end = i + len/2;
+        }
+    }
+    return s.substr(start, end - start + 1); // return substring from start to end
 }
 
 int main() {
@@ -27,3 +52,6 @@ int main() {
 
     return 0;
 }
+
+// TC = O(n^2) , where there are n possible centers
+// SC = O(1)
