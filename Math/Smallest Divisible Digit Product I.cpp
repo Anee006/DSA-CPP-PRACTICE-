ad738d8af926bc3e1d3cc3e866a15f9c08ef67a5
Anee006 <aneesha.sibyj@gmail.com>
2026-08-06 17:42:06 +0530

    smallest divisible digit product I

--- Math/Smallest Divisible Digit Product I.cpp
+++ Math/Smallest Divisible Digit Product I.cpp
@@ -17,7 +17,23 @@ that satisfies the condition.
 #include <iostream>
 using namespace std;
 
+int prod(int num) {
+    if(num == 0) return 0;
+
+    int product = 1;
+
+    while(num > 0) {
+        product *= num % 10;
+        num /= 10;
+    }
+    return product;
+}
+
 int smallestNumber(int n, int t) {
+    while(true) {
+        if(prod(n) % t == 0) return n;
+        n++;
+    }
 }
 
 int main() {
