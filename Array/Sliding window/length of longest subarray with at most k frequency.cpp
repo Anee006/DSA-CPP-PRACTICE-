7b8a20a46fa502bf6a2b95d5f68a8b0e747720ff
Anee006 <aneesha.sibyj@gmail.com>
2026-08-12 19:56:22 +0530

    length of longest subarray with at most k frequency

---
+++ Array/Sliding window/length of longest subarray with at most k frequency.cpp
@@ -0,0 +1,36 @@
+// LeetCode 2958
+// Using hash map + sliding window
+
+// You are given an integer array nums and an integer k.
+// The frequency of an element x is the number of times it occurs in an array.
+// An array is called good if the frequency of each element in this array is less than or equal to k.
+// Return the length of the longest good subarray of nums.
+
+/*
+Input: nums = [1,2,3,1,2,3,1,2], k = 2
+Output: 6
+Explanation: The longest possible good subarray is [1,2,3,1,2,3] since the values 1, 2, and 3 occur at most twice in this subarray. 
+Note that the subarrays [2,3,1,2,3,1] and [3,1,2,3,1,2] are also good.
+*/
+
+// LOGIC:
+// left -> stores start of the current window, right -> expands the window. freq[x] -> number of times x occurs in the current window.
+// Whenever adding nums[right] makes its frequency greater than k, the window becomes invalid. 
+// Move left forward until the frequency becomes valid again.
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int maxSubarrayLength(vector<int>& nums, int k) {
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 1, 2, 3, 1, 2};
+    int k = 2;
+
+    cout << maxSubarrayLength(nums, k);
+
+    return 0;
+}
