93578d1181995463b2be611fe5761124c09b60b9
Anee006 <aneesha.sibyj@gmail.com>
2026-08-15 18:26:30 +0530

    plus one

--- Array/plus one.cpp
+++ Array/plus one.cpp
@@ -9,9 +9,9 @@ Input: digits = [1,2,3]
 Output: [1,2,4]
 Explanation: The array represents the integer 123. Incrementing by one gives 123 + 1 = 124. Thus, the result should be [1,2,4].
 
-Input: digits = [9]
-Output: [1,0]
-Explanation: The array represents the integer 9. Incrementing by one gives 9 + 1 = 10. Thus, the result should be [1,0].
+Input: digits = [9, 9]
+Output: [1,0, 0]
+Explanation: The array represents the integer 99. Incrementing by one gives 99 + 1 = 100. Thus, the result should be [1,0, 0].
 */
 
 #include <iostream>
@@ -19,6 +19,21 @@ Explanation: The array represents the integer 9. Incrementing by one gives 9 + 1
 using namespace std;
 
 vector<int> plusOne(vector<int>& digits) {
+    // start from the right-most digit (just like how we do in normal addition)
+    for(int i = digits.size() - 1; i >= 0; i--) {
+
+        if(digits[i] < 9) { // if curr digit < 9 --> increment it by 1
+            digits[i]++; // increment the last digit
+            return digits;
+        }
+
+        digits[i] = 0; // if curr digit = 9, it becomes 0 and carry moves to left (just like in normal addition)
+    }
+
+    // if we reach here, every digit was 9 (eg: 99 will become 100)
+    digits.insert(digits.begin(), 1); // insert 1 at beginning (to make 99 as 100)
+
+    return digits;
 }
 
 int main() {
@@ -31,3 +46,5 @@ int main() {
     return 0;
 }
 
+// TC = O(9)
+// SC = O(1)
