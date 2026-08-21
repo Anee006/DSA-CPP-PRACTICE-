a390e19330392786d4469ce37ebfbec6ec83465b
Anee006 <aneesha.sibyj@gmail.com>
2026-08-21 09:01:38 +0530

    convert string to lowercase: using in-built lower-case fn

--- String/Convert String to Lower Case.cpp
+++ String/Convert String to Lower Case.cpp
@@ -12,6 +12,11 @@ Explanation: A, B, C and E are converted to a, b, c and e thus all uppercase cha
 using namespace std;
 
 string toLower(string& s) {
+    for(char& c : s) { // pass by reference so that original string is modified
+        c = tolower(c); // inbuilt fn to convert str to lower case
+    }
+
+    return s; // modified string
 }
 
 int main() {
