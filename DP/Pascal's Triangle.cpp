87ee86f9f4e6a8f82a2a44e25718228b78b40116
Anee006 <aneesha.sibyj@gmail.com>
2026-08-21 12:07:40 +0530

    Pascal's triangle

---
+++ DP/Pascal's Triangle.cpp
@@ -0,0 +1,34 @@
+// LeetCode 118
+
+// Given an integer numRows, return the first numRows of Pascal's triangle. 
+// In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+// Input: numRows = 5
+// Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+// LOGIC:
+// In Pascal's Triangle, the first and last elements of every row are always 1. 
+// Every element at [i][j] (i -> row, j -> col) is the sum of the two elements directly above it: triangle[i-1][j-1] + triangle[i-1][j].
+// triangle[i-1][j-1] -> diagonally left, triangle[i-1][j] -> directly above 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> generate(int numRows) {
+}
+
+int main() {
+    int numRows = 5;
+
+    vector<vector<int>> ans = generate(numRows);
+
+    for(int i = 0; i < ans.size(); i++) {
+        for(int j = 0; j < ans[i].size(); j++) {
+            cout << ans[i][j] << endl;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
