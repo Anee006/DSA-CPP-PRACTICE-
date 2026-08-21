10a12e527dd0ace2a22b70b796c7528d7edc931e
Anee006 <aneesha.sibyj@gmail.com>
2026-08-21 11:36:36 +0530

    binary substrings with corners as 1

---
+++ String/Binary Substrings with Corners as 1.cpp
@@ -0,0 +1,28 @@
+// geeksforgeeks
+
+// Given a binary string s. You have to count the number of substrings that start and end with 1.
+
+/*
+Input: s = "01101"
+Output: 3
+Explanation: There are 3 substrings from the given string. They are "11", "101", "1101".
+*/
+
+// LOGIC:
+// Using mathematical approach:
+// If there are "m" total '1's in the string :Any pair of '1's (one starting and one ending) defines a unique valid substring.
+// The number of ways to choose 2 distinct '1's is given by the combination formula: (m * (m-1)) / 2
+
+#include <iostream>
+using namespace std;
+
+int binarySubstring(string& s) {
+}
+
+int main() {
+    string s = "01101";
+    
+    cout << binarySubstring(s);
+
+    return 0;
+}
