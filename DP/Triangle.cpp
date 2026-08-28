6ac20d2f9100b016f0a8aee59affd9d4434b6927
Anee006 <aneesha.sibyj@gmail.com>
2026-08-28 09:38:20 +0530

    triangle: solved using bottom-up DP

--- DP/Triangle.cpp
+++ DP/Triangle.cpp
@@ -1,4 +1,5 @@
 // LeetCode 120
+// Using bottom-up DP
 
 // Given a triangle array, return the minimum path sum from top to bottom.
 // For each step, you may move to an adjacent number of the row below. 
@@ -20,6 +21,16 @@ The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above)
 using namespace std;
 
 int minimumTotal(vector<vector<int>>& triangle) {
+    int n = triangle.size();
+
+    // start from 2nd last row (n-2) and move to top row(0)
+    for(int row = n-2; row >= 0; row--) {
+        for(int col = 0; col <= row; col++) {
+            triangle[row][col] += min(triangle[row+1][col], triangle[row+1][col+1]); // based on given condition
+        }
+    }
+
+    return triangle[0][0]; // top element hold the min path sum
 }
 
 int main() {
@@ -29,3 +40,6 @@ int main() {
 
     return 0;
 }
+
+// TC = O(N^2)
+// SC = O(1)
