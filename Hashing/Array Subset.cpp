2f6989d3f65aba87a5950dca216fe660edc70633
Anee006 <aneesha.sibyj@gmail.com>
2026-09-07 18:37:27 +0530

    Array subset: solved using hashmap

--- Hashing/Array Subset.cpp
+++ Hashing/Array Subset.cpp
@@ -8,12 +8,27 @@
 
 // Note: elements in "b" can occur in any order in "a"
 
+// LOGIC:
+// check whether every element of "b" occurs in "a"
+
 #include <iostream>
 #include <vector>
 #include <unordered_map>
 using namespace std;
 
 bool isSubset(vector<int>& a, vector<int>& b) {
+    unordered_map<int, int> freq; // {num, freq}
+
+    for(int num : a) freq[num]++;  // count freq of elements in "a"
+
+    // check whether "b" can be formed from "a"
+    for(int x : b) {
+        if(freq[x] == 0) return false; // b does not exist in a
+
+        freq[x]--; // decrease freq to handle duplicate values in "b"
+    }
+
+    return true;
 }
 
 int main() {
@@ -25,3 +40,6 @@ int main() {
 
     return 0;
 }
+
+// TC = O(n + m) , where n = a.size() and m = b.size()
+// SC = O(n) (for hash map)
