495af7396d8bf901cf05936e4a5aa10181cf94f1
Anee006 <aneesha.sibyj@gmail.com>
2026-09-08 13:42:05 +0530

    Count commas in a range

---
+++ Math/Count Commas in Range.cpp
@@ -0,0 +1,25 @@
+// LeetCode 3870
+
+// You are given an integer n.
+// Return the total number of commas used when writing all integers from [1, n] (inclusive) in standard number formatting.
+// In standard formatting:
+// A comma is inserted after every three digits from the right. Numbers with fewer than 4 digits contain no commas.
+
+// Input: n = 1002
+// Output: 3
+// Explanation: The numbers "1,000", "1,001", and "1,002" each contain one comma, giving a total of 3
+
+#include <iostream>
+using namespace std;
+
+int countCommas(int n) {
+
+}
+
+int main() {
+    int n = 1002;
+
+    cout << countCommas(n);
+
+    return 0;
+}
