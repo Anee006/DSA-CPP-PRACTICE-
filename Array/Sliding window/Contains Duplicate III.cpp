5abf55d329fe390efe60f3de6f1c877928090027
Anee006 <aneesha.sibyj@gmail.com>
2026-09-10 18:51:31 +0530

    Contains Duplicate III: solved using sliding window + lower_bound()

--- Array/Sliding window/Contains Duplicate III.cpp
+++ Array/Sliding window/Contains Duplicate III.cpp
@@ -17,12 +17,34 @@ abs(i - j) <= indexDiff --> abs(0 - 3) <= 3
 abs(nums[i] - nums[j]) <= valueDiff --> abs(1 - 1) <= 0
 */
 
+// LOGIC:
+// for indexDiff -> use sliding window
+// for valueDiff -> use lower_bound() --> finds smallest val in set that is >= nums[i]
+// At index i, window contains only: nums[i-indexDiff] ... nums[i-1] 
+// So every element in window automatically satisfies: abs(i - j) <= indexDiff.
+// nums[i] - valueDiff <= nums[j] <= nums[i] + valueDiff , which is exactly: abs(nums[i] - nums[j]) <= valueDiff
+
 #include <iostream>
 #include <set>
 #include <vector>
 using namespace std;
 
 bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) {
+    set<int> s;
+
+    for(int i = 0; i < nums.size(); i++) {
+        // remove elements that are outside indexDiff window
+        if(i > indexDiff) s.erase(nums[i - indexDiff - 1]);
+
+        // Find first number >= nums[i] - valueDiff
+        auto it = s.lower_bound(nums[i] - valueDiff);
+
+        // check if that num is <= nums[i] + valueDiff
+        if(it != s.end() && *it <= nums[i] + valueDiff) return true;
+
+        s.insert(nums[i]);
+    }
+    return false;
 }
 
 int main() {
@@ -33,5 +55,8 @@ int main() {
 
     return 0;
 }
+
+// TC = O(n log(indexDiff))
+// SC = O(indexDiff)
 
  
