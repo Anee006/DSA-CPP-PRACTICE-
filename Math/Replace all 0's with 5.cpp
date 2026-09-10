ff01c66cc5501c746a6dfa1af0c921f2e4bcd126
Anee006 <aneesha.sibyj@gmail.com>
2026-09-10 19:08:06 +0530

    Replace all 0's with 5

---
+++ Math/Replace all 0's with 5.cpp
@@ -0,0 +1,21 @@
+// geeksforgeeks
+
+// You are given an integer n. You need to convert all zeroes of n to 5.
+
+// Input: n = 1004
+// Output: 1554
+// Explanation: There are two zeroes in 1004 on replacing all zeroes with 5, the new number will be 1554.
+
+#include <iostream>
+using namespace std;
+
+int convertFive(int n) {
+}
+
+int main() {
+    int n = 1004;
+
+    cout << convertFive(n);
+
+    return 0;
+}
