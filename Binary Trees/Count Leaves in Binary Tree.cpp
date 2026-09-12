18410f722fbaccbccc422c752daaa970684cae8c
Anee006 <aneesha.sibyj@gmail.com>
2026-09-12 18:09:43 +0530

    Count leaves in a binary tree

---
+++ Binary Trees/Count Leaves in Binary Tree.cpp
@@ -0,0 +1,57 @@
+// geeksforgeeks
+
+// Given a Binary Tree of size n, You have to count leaves in it. 
+
+// input: {1, 10, 39, 5}
+// output: 2
+// explanation: 5 and 39 are the two leaf nodes.
+
+// logic:
+// leaf is a node whose left and right child both are NULL.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+
+// build BT using preorder sequence
+Node* buildBT(vector<int>& pre) {
+    idx++;
+
+    Node* root = new Node(pre[idx]);
+
+    if(pre[idx] == -1) return NULL;
+
+    root->left = buildBT(pre);
+    root->right = buildBT(pre);
+
+    return root;
+}
+
+int countLeaves(Node* root) {
+}
+
+int main() {
+    vector<int> pre = {1, 10, 5, -1, -1, -1, -1, 39, -1, -1};
+
+    Node* root = buildBT(pre);
+
+    cout << countLeaves(root);
+
+    return 0;
+}
+
+
