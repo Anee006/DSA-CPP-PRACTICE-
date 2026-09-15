41338f7b301b6fc8f2c483b22c2256fcf4fab66b
Anee006 <aneesha.sibyj@gmail.com>
2026-09-15 21:53:48 +0530

    Number of 1 bits

---
+++ Bit Manipulation/Number of 1 Bits.cpp
@@ -0,0 +1,41 @@
+// LeetCode 191
+
+// Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
+
+/*
+Input: n = 11
+Output: 3
+Explanation:
+The input binary string 1011 has a total of three set bits.
+
+Working:
+n = 11 = 1011 
+n - 1 = 10 = 1010
+1011 & 1010 = 1010 i.e 10
+count = 1
+
+n = 10 = 1010,  n-1 = 9 = 1001
+1010 & 1001 = 1000 i.e 8
+count = 2
+
+n = 8 = 1000, n-1 = 7 = 0111
+1000 & 0111 = 0
+count = 3 
+
+So, there are 3 set bits in 11.
+*/
+
+#include <iostream>
+using namespace std;
+
+int hammingWeight(int n) {
+
+}
+
+int main() {
+    int n = 11;
+
+    cout << hammingWeight(n);
+
+    return 0;
+}
