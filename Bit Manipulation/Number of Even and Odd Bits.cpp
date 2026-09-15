bd86500b5875e893f3ea74d5a2c87c397a517b61
Anee006 <aneesha.sibyj@gmail.com>
2026-09-15 22:06:31 +0530

    No. of even and odd bits

---
+++ Bit Manipulation/Number of Even and Odd Bits.cpp
@@ -0,0 +1,30 @@
+// LeetCode 2595
+
+// You are given a positive integer n. Let even denote the number of even indices in the binary representation of n with value 1.
+// Let odd denote the number of odd indices in the binary representation of n with value 1.
+// Note that bits are indexed from right to left in the binary representation of a number. Return the array [even, odd].
+
+/*
+Input: n = 50
+Output: [1,2]
+Explanation:
+The binary representation of 50 is 110010. It contains 1 on indices 1, 4, and 5.
+So even index = 1, odd indices = 2
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> evenOddBit(int n) {
+}
+
+int main() {
+    int n = 50;
+
+    vector<int> ans = evenOddBit(n);
+
+    for(int x : ans) cout << x << " ";
+
+    return 0;
+}
