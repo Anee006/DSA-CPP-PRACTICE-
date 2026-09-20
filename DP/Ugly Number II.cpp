c1dcc84fe18ab7ec61fd286e2581d42c06b97c0a
Anee006 <aneesha.sibyj@gmail.com>
2026-09-20 16:35:29 +0530

    Ugly Number II

---
+++ DP/Ugly Number II.cpp
@@ -0,0 +1,22 @@
+// LeetCode 264
+
+// An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5. Given an integer n, return the nth ugly number.
+
+// Input: n = 10
+// Output: 12
+// Explanation: [1, 2, 3, 4, 5, 6, 8, 9, 10, 12] is the sequence of the first 10 ugly numbers.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int nthUglyNumber(int n) {
+}
+
+int main() {
+    int n = 10;
+
+    cout << nthUglyNumber(n);
+    
+    return 0;
+}
